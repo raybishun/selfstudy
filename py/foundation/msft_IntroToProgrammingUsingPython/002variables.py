072d98419fab3b31c10d66c87a740db438fb866d
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-01-06 20:41:10 -0500

    Added Conditional and Iteration modules

--- py/foundation/msft_IntroToProgrammingUsingPython/002variables.py
+++ py/foundation/msft_IntroToProgrammingUsingPython/002variables.py
@@ -1,13 +1,13 @@
-name = "Ray Bishun";
-ctr = 100;
+name = "Ray Bishun"
+ctr = 100
 
-print("ctr is ", ctr);
+print("ctr is ", ctr)
 
-a = b = c = ctr;
-print(a, b, c, ctr);
+a = b = c = ctr
+print(a, b, c, ctr)
 
-a, b, c = ctr, ctr, name;
-print(a, b, c);
+a, b, c = ctr, ctr, name
+print(a, b, c)
 
 # =============================================================================
 # Python Datatypes
@@ -21,60 +21,60 @@ print(a, b, c);
 # =============================================================================
 # Numbers
 # =============================================================================
-var1 = 2000;
-print(var1);
-del var1;
+var1 = 2000
+print(var1)
+del var1
 # print(var1);
 
 # =============================================================================
 # Strings
 # =============================================================================
-name = "Ray Bishun";
-greeting = "Hello, ";
-print(name[0]);
-print(name[4:10]);
-print(greeting + name);
-print(greeting * 3, name);
+name = "Ray Bishun"
+greeting = "Hello, "
+print(name[0])
+print(name[4:10])
+print(greeting + name)
+print(greeting * 3, name)
 
 # =============================================================================
 # Lists
 # =============================================================================
-list = ['Hello World', 150, 1.25, "Ray Bishun", 100.25];
-tinyList = [300, 'Ray'];
+list = ['Hello World', 150, 1.25, "Ray Bishun", 100.25]
+tinyList = [300, 'Ray']
 
-print(list);
-print(tinyList * 2);
-print(tinyList[1]);
+print(list)
+print(tinyList * 2)
+print(tinyList[1])
 
-nums = ["one", "two", "three", "four", "five"];
-print(nums[2:3]);
+nums = ["one", "two", "three", "four", "five"]
+print(nums[2:3])
 
 # =============================================================================
 # Tuples (think of tuples as read-only lists)
 # =============================================================================
-sample_tuple = ('Hello Ray', 1.618, .618, 'Hello again', 133);
-print(sample_tuple[1]);
+sample_tuple = ('Hello Ray', 1.618, .618, 'Hello again', 133)
+print(sample_tuple[1])
 # sample_tuple[0] = 'hi'; # TypeError: 'tuple' object does NOT support item assignment
 
 # =============================================================================
 # Dictionary
 # =============================================================================
-print("---------------------------");
-dictionary = {};
-dictionary['one'] = "first element";
-dictionary[2] = "second element";
+print("---------------------------")
+dictionary = {}
+dictionary['one'] = "first element"
+dictionary[2] = "second element"
 
-tiny_dictionary = {'name': 'Ray', 'age': '25', 'id': '618', 'role': 'developer'};
-print(dictionary['one']);
-print(tiny_dictionary['id']);
-print(tiny_dictionary.keys());
-print(tiny_dictionary.values());
+tiny_dictionary = {'name': 'Ray', 'age': '25', 'id': '618', 'role': 'developer'}
+print(dictionary['one'])
+print(tiny_dictionary['id'])
+print(tiny_dictionary.keys())
+print(tiny_dictionary.values())
 
 # =============================================================================
 # DataType Conversion
 # =============================================================================
-print(int('20')+10);
-print(float('1.618'));
+print(int('20')+10)
+print(float('1.618'))
 
 # =============================================================================
 # Variable Operators
