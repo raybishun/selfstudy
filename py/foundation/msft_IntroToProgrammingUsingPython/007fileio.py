aa60f0ca67d60fa8cbc8c1616fc53fe3358d4853
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-01-09 21:23:54 -0500

    Added 007fileio.py

--- py/foundation/msft_IntroToProgrammingUsingPython/007fileio.py
+++ py/foundation/msft_IntroToProgrammingUsingPython/007fileio.py
@@ -5,5 +5,30 @@
 #   3. read & write
 #   4. append mode
 # =============================================================================
-str = input("Enter name: ")
-print("Hello", str)
+# stdin
+name = input("Enter name: ")
+# stdout
+print("Hello", name)
+
+# =============================================================================
+# Write to file
+# =============================================================================
+f = open("007fileio.txt", "w")
+
+# Get file stats
+print("file name:", f.name)
+print("file closed?", f.closed)
+print("file mode:", f.mode)
+
+f.write(name)
+f.close()
+
+# =============================================================================
+# Read from file
+# =============================================================================
+f = open("007fileio.txt", "r")
+output = f.read(1)
+print(output)
+f.close()
+
+print("file mode:", f.mode)
