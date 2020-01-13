184efa00c186b0e7032b7502e54b89682a116138
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-01-13 17:29:32 -0500

    Renamed parent folder to u_IntroToProgrammingUsingPython

--- cs/ref/ref48/ref48/Program.cs
+++ cs/ref/ref48/ref48/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ref48
 {
@@ -16,11 +12,16 @@ namespace ref48
         static void Greeting()
         {
             // Summary:
-            //      C# source code is compiled to Common Intermediate Language (CIL, aka IL, MSIL).
-            //      Just-in-time compilation compiles the code to Native Machine Code (based on 
-            //      the computer's architecture).
-            //      The Common Language Runtime (CLR) executes this Native Machine Code.
-            //      The CLR provides security and memory management for the code it executes.
+            //  C# source code is compiled to Common
+            //      Intermediate Language
+            //      (CIL, aka IL, MSIL).
+            //  Just-in-time compilation compiles
+            //      the code to Native Machine Code
+            //      (based on the computer's architecture).
+            //  The Common Language Runtime (CLR) executes
+            //      this Native Machine Code.
+            //  The CLR provides security and memory
+            //      management for the code it executes.
             
             Console.WriteLine("Hello, World!");
         }
