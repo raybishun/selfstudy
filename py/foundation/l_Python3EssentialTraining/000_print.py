4119a0c40d624d6e19afae72ee777106ec45dd1c
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-01-14 20:50:28 -0500

    Updated 000_scripts

--- py/foundation/l_Python3EssentialTraining/000_print.py
+++ py/foundation/l_Python3EssentialTraining/000_print.py
@@ -3,11 +3,11 @@
 # =============================================================================
 age = 25
 
-# In Python 2
+# Python 2
 print('the integer is: %d' % age) # where d = integer
 
-# In Python 3
+# Python 3
 print('the integer is: {}'.format(age))
 
-# In Python 3.6
+# Python 3.6 introduced the 'f string'
 print(f'the integer is: {age}') # where f = format
