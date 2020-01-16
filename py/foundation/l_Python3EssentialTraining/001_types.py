56b0ea9bc3f25f52f339fb4dadfdcb0720a235ce
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-01-15 20:28:02 -0500

    Added float and decimal to types.py

--- py/foundation/l_Python3EssentialTraining/001_types.py
+++ py/foundation/l_Python3EssentialTraining/001_types.py
@@ -19,14 +19,40 @@ print(type(x), type(y))
 z = 'switched and 0s added "{1:<09}" "{0:>09}"'.format("zero", 'one')
 print('z {}'.format(z))
 
+# =============================================================================
+# Numeric types (integers and floats)
+# =============================================================================
+print('\x1b[2J')
+
+x = 7 / 3
+print(f'{x}')
+print(type(x))
+
+x = 7 // 3 # // returns only the int
+print(f'{x}')
+print(type(x))
+
+x = 7 % 3 # // modulo (the %) returns remainder
+print(f'{x}')
+print(type(x))
+
+# float NOT for use with money !!!
+# below example shows why, and it's due to accuracy vs precision
+# where decimal returns accuracy (for use with money)
+# but float return floating point accuracy
+x = .10 + .10 + .10 - .30
+print(f'{x}')
+print(type(x))
 
 # =============================================================================
-# Numeric types
+# Decimal
 # =============================================================================
-
-# =============================================================================
-# 
-# =============================================================================
+from decimal import *
+a = Decimal('.10')
+b = Decimal('.30')
+x = a + a + a - b
+print(f'{x}')
+print(type(x))
 
 # =============================================================================
 # 
