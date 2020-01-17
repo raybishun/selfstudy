4903e88774ac80172f298e1a06cef7477c5dedea
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-01-16 20:45:55 -0500

    Updated 001_types.py

--- py/foundation/l_Python3EssentialTraining/001_types.py
+++ py/foundation/l_Python3EssentialTraining/001_types.py
@@ -155,3 +155,39 @@ for i in dict:
 
 for k,v in dict.items():
     print(f'{k}:{v}')
+
+# =============================================================================
+# type() and id()
+# =============================================================================
+x = (1, 2, 3, 34, 5)
+print(f'{x}')
+print(type(x))
+
+x = (1, 'two', 3.0, [4, 'four'], 5)
+print(f'{x}')
+print(type(x[1]))
+
+# id()
+x = (1, 'two', 3.0, [4, 'four'], 5)
+y = (1, 'two', 3.0, [4, 'four'], 5)
+print(f'{x}')
+print(id(x))
+print(id(y))
+
+if x is y:
+    print('yes')
+else:
+    print('no')
+
+if x[0] is y[0]:
+    print('yes')
+else:
+    print('no')
+
+# isinstance
+if isinstance(x, tuple):
+    print('is a tuple')
+elif isinstance(x, list):
+    print('is a list')
+else:
+    print('no')
