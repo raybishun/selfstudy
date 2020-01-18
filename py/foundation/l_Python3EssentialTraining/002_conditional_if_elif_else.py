41a167f9886ec735a6b347c27f4de9980e03959c
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-01-17 23:08:25 -0500

    Added 002_*.py scripts

---
+++ py/foundation/l_Python3EssentialTraining/002_conditional_if_elif_else.py
@@ -0,0 +1,19 @@
+# =============================================================================
+# Conditionals
+# =============================================================================
+from datetime import date
+
+today = date.today().weekday()
+
+if today == 0:
+    print('Monday')
+elif today == 1:
+    print('Tuesday')
+elif today == 2:
+    print('Wednesday')
+elif today == 3:
+    print('Thursday')
+elif today == 4:
+    print('Friday')
+else:
+    print('Unknown')
