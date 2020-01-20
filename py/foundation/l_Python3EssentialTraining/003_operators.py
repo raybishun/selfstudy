4eea9b18fa2cb79d4f563b24a59246376ba5548d
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-01-20 09:46:31 -0500

    Updated 003_operators.py

--- py/foundation/l_Python3EssentialTraining/003_operators.py
+++ py/foundation/l_Python3EssentialTraining/003_operators.py
@@ -13,9 +13,44 @@
     //  Integer Division
     %   Modulo (remainder)
     **  Exponent
-    -   Unary negative
-    +   Unary positive
+    -   Unary negative 
+    +   Unary positive 
+
+    NOTE: Unary does not actually change the value of the operand.
+          It seems to be their for descriptive purposes.
 """
+
+# Division (retuns a float)
+x = 5
+y = 3
+z = x / y
+print(f'{type(z)}, {z}')
+
+# Division - return integer
+x = 5
+y = 3
+z = x // y
+print(f'{type(z)}, {z}')
+
+# Division - return remainder (aka modulo) as an integer
+x = 5
+y = 3
+z = x % y
+print(f'{type(z)}, {z}')
+
+# Unary (negative)
+x = 5
+y = 3
+z = x + y
+z = -z
+print(f'{type(z)}, {z}')
+
+# Unary (positive)
+x = 5
+y = 3
+z = x + y
+z = +z
+print(f'{type(z)}, {z}')
 
 # -----------------------------------------------------------------------------
 # Bitwise Operators
