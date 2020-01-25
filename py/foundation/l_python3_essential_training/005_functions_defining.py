91f4ccf2138e433d3ab5bd9075c2bc22135f5504
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-01-24 22:29:11 -0500

    Updated 005_functions_*.py

--- py/foundation/l_python3_essential_training/005_functions_defining.py
+++ py/foundation/l_python3_essential_training/005_functions_defining.py
@@ -1,16 +1,20 @@
 # =============================================================================
-# Functions
+# Defining Functions
 # =============================================================================
 """
-    All functions return a value.
+    All functions return a value, ('none' by default)
 """
+import os
+
 
 def main():
-    greeting()
+    greeting('Hello')
 
-def greeting():
-    print('Hello World!')
+def greeting(str):
+    print(f'{str}, {os.getlogin()}')
 
-# the special variable __name__ will return the name of this (current) module
-# however, this is not an imported module, rather this is the 'main' file
+# the special variable __name__ will return the name of the current imported 
+# module, however, since in this case this script is not an imported module, 
+# and is running as the main unit of execution, __main__ takes on a special 
+# role, denoting that this is the 'main' file
 if __name__ == '__main__': main()
