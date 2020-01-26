19d1716789de492e22570c4450d34561c8f89478
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-01-26 17:31:38 -0500

    Updated 005_functions_*.py

--- py/foundation/l_python3_essential_training/000_print.py
+++ py/foundation/l_python3_essential_training/000_print.py
@@ -11,3 +11,8 @@ print('the integer is: {}'.format(age))
 
 # Python 3.6 introduced the 'f string'
 print(f'the integer is: {age}') # where f = format
+
+# Print on the same line
+nums = [1, 2, 3]
+for num in nums:
+    print(num, end=' ')
---
+++ py/foundation/l_python3_essential_training/005_functions_args_variable_len_args.py
@@ -0,0 +1,24 @@
+# =============================================================================
+# Functions: Variable-length arguments
+# =============================================================================
+print('\x1b[2J')
+
+def add(*args):
+    if len(args):
+        for arg in args:
+            print(arg, end=' ')
+        print()
+    else:
+        print("Nothing was passed in.")
+
+def main():
+    add(1, 2, 3)
+    add(1)
+    add(1, 2, 3, 4, 5)
+    add()
+
+    # Pass a tuple
+    x = (1, 2, 3)
+    add(*x)
+
+if __name__ == '__main__': main()
