61b2c8a12900600aa24c29bdbd5858fb8c954461
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-01-27 19:57:34 -0500

    Updated 005_functions_args_keyword_args.py

--- py/foundation/l_python3_essential_training/005_functions_args_keyword_args.py
+++ py/foundation/l_python3_essential_training/005_functions_args_keyword_args.py
@@ -1,11 +1,16 @@
 # =============================================================================
-# Functions: Keyword Arguments
+# Functions: KeyWord Arguments (aka Named Arguments)
 # =============================================================================
 print('\x1b[2J')
 
-
+def nums(**kwargs):
+   if len(kwargs):
+      for k in kwargs:
+         print('Num {} is {}'.format(k, kwargs[k])) 
+   else:
+      print('done.')
 
 def main():
-   print()
+   nums(One = 1, Two = 2, Three = 3)
 
 if __name__ == '__main__': main()
