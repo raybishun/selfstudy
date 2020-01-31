eaa850d0dab0effe392019f754c167e0e47a5c2e
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-01-30 20:58:15 -0500

    Updated 005_functions_decorators.py

--- py/foundation/l_python3_essential_training/005_functions_decorators.py
+++ py/foundation/l_python3_essential_training/005_functions_decorators.py
@@ -2,5 +2,54 @@
 # Function Decorators
 # =============================================================================
 """
+    Decorators return a 'wrapper function'
     
+    ...seems similar to delegates in C#
 """
+
+# -----------------------------------------------------------------------------
+# Function Overview #1
+# -----------------------------------------------------------------------------
+def f1():
+    print ('Hello from f1')
+
+# call f1
+f1()
+
+# call calls f1
+x = f1
+x()
+
+print()
+# -----------------------------------------------------------------------------
+# Function Overview #2
+# -----------------------------------------------------------------------------
+def f1():
+    def f2():
+        print('Hello from f2 ')
+    return f2
+x = f1()
+x()
+
+print()
+print()
+# -----------------------------------------------------------------------------
+# ...now the decorator in action...
+# -----------------------------------------------------------------------------
+def f1(f):
+    def f2():
+        print('Hello from f2 - BEFORE the function call')
+        f()
+        print('Hello from f2 - AFTER the function call')
+    return f2
+
+@f1 # the decorator
+def f3():
+    print('Hello from f3')
+
+# because of the above decorator, we don't need the next line
+#f3 = f1(f3)
+f3()
+
+print()
+
