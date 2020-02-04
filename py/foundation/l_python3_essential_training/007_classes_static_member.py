608ee3e32887c2e34a67135df815685e16ae65c9
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-02-03 22:33:28 -0500

    Added 007_classes_static_member.py

---
+++ py/foundation/l_python3_essential_training/007_classes_static_member.py
@@ -0,0 +1,47 @@
+# =============================================================================
+# Classes: Static Member
+# =============================================================================
+class Asset:
+
+    account = ['Ray', '1618', 'NY'] # This seems similar to a static member in C#
+
+    def __init__(self, **kwargs):
+        self._type = kwargs['type'] if 'type' in kwargs else 'Index'
+        self._symbol = kwargs['symbol'] if 'symbol' in kwargs else 'SP500'
+        self._close = kwargs['close'] if 'close' in kwargs else 0.00
+
+    def type(self, t=None):
+        if t: self._type = t
+        return self._type
+    
+    def symbol(self, s=None):
+        if s: self._symbol = s
+        return self._symbol
+
+    def close(self, c=None):
+        if c: self._close = c
+        return self._close
+
+    def __str__(self):
+        return f'{self.type()} \t{self.symbol()} \t{self.close()}'
+    
+def print_asset(o):
+    if not isinstance(o, Asset):
+        raise TypeError('print_asset(): requires an Asset')
+    print(f'TYPE: {o.type()} \tSYMBOL: {o.symbol()} \tCLOSING_PRICE: {o.close()}')
+
+def main():
+    msft = Asset(type='Stock', symbol='MSFT', close=170.23)
+    spx = Asset(type='Option', symbol='SPX', close=3225.52)
+
+    msft.close(200.00)
+
+    print(msft)
+    print(spx)
+
+    # Static member PoC
+    print(msft.account)
+    spx.account[1] = 618
+    print(msft.account)
+
+if __name__ == '__main__': main()
