3f458a82cab3efa0f382cc99cf7d6038b220d3f9
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-02-07 21:29:09 -0500

    Added SignalTime

---
+++ cs/foundation/dotnetcore/SignalTime/SignalTime/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Timers;
+
+
+namespace SignalTime
+{
+    class Program
+    {
+        static Timer timer;
+        static void Main(string[] args)
+        {
+            DoWork(1000);
+            Console.WriteLine("Press any key to abort.\n");
+            Console.ReadKey();
+        }
+
+        static void DoWork(double seconds)
+        {
+            try
+            {
+                timer = new Timer(seconds);
+                timer.Elapsed += OnTimerEvent;
+                timer.AutoReset = true;
+                timer.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                timer.Enabled = false;
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void OnTimerEvent(object sender, ElapsedEventArgs e)
+        {
+            // Get the day/time when the Timer.Elapsed event was raised.
+            Console.WriteLine($"{e.SignalTime}.");
+        }
+    }
+}
