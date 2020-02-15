1a97b8fb2704d080c6a7048469331b735a260467
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-02-15 00:05:27 -0500

    010_file*.py

---
+++ py/foundation/l_python3_essential_training/010_file_io.py
@@ -0,0 +1,22 @@
+# =============================================================================
+# File: I/O
+# =============================================================================
+
+def main():
+    # open file for reading (the default if not specified)
+    # modes include: r=reading, w=writing, a=append
+    # also:
+        # r+ = read and write
+        # w+ = write and read
+        # a+ = append and read
+    # and:
+        # r+b = read binary
+        # r+t = read text
+        # w+b = write binary
+        # w+t = write text
+    f = open('010_file_io.txt', 'r')
+    for line in f:
+        print(line.rstrip())
+
+
+if __name__ == '__main__': main()
