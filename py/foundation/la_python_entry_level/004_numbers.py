7f8bfef2cfc7ef27d7cfc138b76ab9a826ba4ec0
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-02-27 20:48:12 -0500

    Added number scripts

---
+++ py/foundation/la_python_entry_level/004_numbers.py
@@ -0,0 +1,12 @@
+
+"""
+    integers
+    floats
+    scientific notation
+"""
+
+# Scientific notation
+print(f'Exponent to the power of 10: {4.5e9}') # note lower case e
+print(f'Exponent to the power of 10: {4.5E9}') # note upper case E
+print(f'4.5e9 equals {4.5 * (10 ** 9)}') # where ** is 'power'
+print(f'{4.5e9 == 4.5 * (10 ** 9)}')
---
+++ py/foundation/la_python_entry_level/005_operators.py
@@ -0,0 +1,20 @@
+# =============================================================================
+# Numeric Operators
+# =============================================================================
+
+# If one of the operands is a float, a float is returned
+print(2 + 1)
+print(2.0 + 1)
+
+# Division (new to Python 3.X, returns a float)
+# aka 'regular division'
+print(5 / 3)
+
+# Floor Division (only returns int)
+print(5 // 3)
+
+# Modulo (returns only the remainder)
+print(5 % 3)
+
+# Exponents
+print(2 ** 3) # 2 x 2 x 2 = 8
