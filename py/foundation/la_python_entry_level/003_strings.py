c8c9674f66e67ef24c06a0f71ebea2b3ad746426
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-02-27 19:47:33 -0500

    Added 003_strings.py

---
+++ py/foundation/la_python_entry_level/003_strings.py
@@ -0,0 +1,30 @@
+# =============================================================================
+# Basics: Strings
+# =============================================================================
+# A string is a collection of tokens
+
+# Multiline string
+"""
+    This is a multiline string
+    It is NOT a comment block
+    As such, this is not a 'free' item
+    Python does NOT have block comments
+"""
+# Misc
+a = 'this is a string'
+b = "this is a string"
+c = '\'this is a string\''
+d = "\"this is a string\""
+e = '\\this is a string\\'
+f = 'hello' + ' world'
+g = "ha " * 4
+print(f'{a}\n{b}\n{c}\n{d}\n{e}')
+print(f)
+print(g)
+print()
+
+# Return index (note, first item is 0)
+print("hello world".find('e'))
+print("hello world".find('ll'))
+print("hello world".find('hi')) # retuns -1 if not found
+print('Hello World'.lower())
