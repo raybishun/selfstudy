5f4c7db2c0f24373d204aa6c36949c0f4e19cca4
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-02-29 17:41:10 -0500

    Added 019_collections_matrices_and_cubes.py

---
+++ py/foundation/la_python_entry_level/019_collections_matrices_and_cubes.py
@@ -0,0 +1,22 @@
+# =============================================================================
+# Collections: Matrices & Cubes
+# =============================================================================
+"""
+    Multidimensional Arrays
+
+    Squares: 2 columns and 2 rows, i.e.:
+        1 2
+        3 4
+
+    Cubes: Same number of columns as rows, i.e.:
+        1 2 3
+        4 5 6
+        7 8 9
+"""
+my_matrix = [[1, 2, 3],
+            [4, 5, 6]]
+print(f'Rows: {len(my_matrix)}')
+print(f'Cols: {len(my_matrix[0])}')
+
+# R & C
+print(my_matrix[1][1]) # returns 5
