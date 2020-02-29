7253c41f8e5a8632cce955499ec688a1bad44838
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-02-29 18:45:13 -0500

    Added 020_collections_tuples.py

---
+++ py/foundation/la_python_entry_level/020_collections_tuples.py
@@ -0,0 +1,21 @@
+# =============================================================================
+# Collections: Tuples
+# =============================================================================
+"""
+    Tuples are immutable
+"""
+
+work_days = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri')
+print(work_days)
+# 'tuple' object does not support item assignment
+# work_days[0] = 'Sun'
+
+# Creating a 'new' tuple
+# Note, for this example, YOU MUST INCLUDE THE COMMA
+# Else, it implies you are attempting some order-of-operations calculation
+week = work_days + ('Sat',) 
+print(week)
+
+# Unpacking tuples into variables
+v, w, x, y, z = work_days
+print(x) # return 'Wed'
