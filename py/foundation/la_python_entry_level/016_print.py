a75cd7774ee094ec605641913bdccff7f0f65ccb
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-02-29 13:38:38 -0500

    Added 016_print.py

---
+++ py/foundation/la_python_entry_level/016_print.py
@@ -0,0 +1,23 @@
+# =============================================================================
+# Print()
+# =============================================================================
+"""
+    print() automatically adds a crlf at the eol
+"""
+
+first_name = input("Enter First Name: ")
+last_name = input("Enter Last Name: ")
+
+# Both on same line (note: uses the default sep=" " for spacing)
+# print(first_name, last_name)
+
+# print(first_name)   # prints on new line
+# print(last_name)    # prints on new line
+
+# Both on same line (using end=" ")
+print(first_name, end=" ")
+print(last_name)
+
+# If not specified in a print statement, sep=" " is used by default
+# Here I'm using a comma to separate the two string tokens
+print(last_name, first_name, sep=", ")
