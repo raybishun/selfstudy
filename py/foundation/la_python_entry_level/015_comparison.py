40b758965e98b54031e76c212723dfcd57c183e5
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-02-29 10:58:19 -0500

    Added 015_comparison.py

---
+++ py/foundation/la_python_entry_level/015_comparison.py
@@ -0,0 +1,24 @@
+# =============================================================================
+# Comparison Operators
+# =============================================================================
+"""
+    <       less than
+    >       greater than
+    <=      less than or equal
+    >=      greater than or equal
+    ==      is equal
+    !=      not equal
+    is      identity operator (checks if same TYPE)
+    is not  checks for opposite TYPES
+"""
+
+# Ord function
+print(ord('a'), ord('A'))
+print(ord('b'), ord('B'))
+print(ord('c'), ord('C'))
+print()
+
+# id
+print(1 is '1')
+print(id(1))
+print(id('1'))
