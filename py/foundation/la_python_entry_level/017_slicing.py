a4dd8c745c6c740be9f34433904ddf98cf56370c
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-02-29 15:37:05 -0500

    Added 017_slicing.py

---
+++ py/foundation/la_python_entry_level/017_slicing.py
@@ -0,0 +1,26 @@
+# =============================================================================
+# Slicing
+# =============================================================================
+"""
+    start:stop:stride
+"""
+py = "Python"
+print(py[1:3]) # returns 'yt'
+
+# Start at 2 (skip 0, 1), and return to the end
+print(py[2:]) # returns 'thon'
+
+# Get length
+print(len(py)) # returns 6
+
+# Return the last character
+print(py[len(py)-1]) # returns 'n'
+
+# Return the last character
+print(py[-1]) # returns 'n'
+
+# Return every other character
+print(py[::2]) # return 'Pto'
+
+# Clever way to reverse order of a string
+print(py[::-1])
