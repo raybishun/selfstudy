158f81e3783aeafbcf5d7906e8689cbd95fc6832
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-02-29 10:40:09 -0500

    Added 014_bool.py

---
+++ py/foundation/la_python_entry_level/014_bool.py
@@ -0,0 +1,16 @@
+# =============================================================================
+# Boolean Operators
+# =============================================================================
+
+# Only requires one Truth to be True :)
+print(f'{True or True}')    # True
+print(f'{True or False}')   # True
+print(f'{False or True}')   # True
+print(f'{False or False}')  # False
+print()
+
+# Both must be True
+print(f'{True and True}')    # True
+print(f'{True and False}')   # False
+print(f'{False and True}')   # False
+print(f'{False and False}')  # False
