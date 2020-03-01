75f5633c21b3b91f3420bbb1df8c76894bb0451b
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-03-01 14:16:10 -0500

    Added 026_flow_control_conditionals_if_elif_else_pass.py

---
+++ py/foundation/la_python_entry_level/026_flow_control_conditionals_if_elif_else_pass.py
@@ -0,0 +1,24 @@
+# =============================================================================
+# Conditionals: if/elif/else/pass
+# =============================================================================
+import datetime as dt
+
+day = dt.datetime.today().day
+
+if day == 1:
+    print('Sun')
+elif day == 2:
+    print('Mon')
+elif day == 3:
+    print('Tue')
+elif day == 4:
+    print('Wed')
+elif day == 5:
+    print('Thu')
+elif day == 6:
+    print('Fri')
+elif day == 7:
+    print('Sat')
+else:
+    # no-op (no operation statement)
+    pass
