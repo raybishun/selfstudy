41cca0ab19c38001d043010024006aa7beec35c0
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-03-10 21:50:08 -0400

    Added 002_classes_structures_interfaces

---
+++ cs/prg_in_cs_1/002_classes_structures_interfaces/002_classes_structures_interfaces/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_classes_structures_interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
