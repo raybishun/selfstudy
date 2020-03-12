0dfd4bb373529eb9424003c3fcc3bc6b235f76fb
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-03-11 21:53:41 -0400

    Updated 002_classes_structures_interfaces

--- cs/prg_in_cs_1/002_classes_structures_interfaces/002_classes_structures_interfaces/Program.cs
+++ cs/prg_in_cs_1/002_classes_structures_interfaces/002_classes_structures_interfaces/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _002_classes_structures_interfaces
 {
@@ -10,17 +6,57 @@ namespace _002_classes_structures_interfaces
     {
         static void Main(string[] args)
         {
-            // Access modifiers
-                // Public
-                // Private
-                // Protected
-                // Internal
-                // Protected internal
-                // Private protected
 
-            // Data Types
-                // Value Type - stack
-                // Reference Type - heap
+            Value_Type_Enum_Types();
+
+            Console.ReadKey();
+        }
+
+        static void Access_Modifiers()
+        {
+            // Public
+            // Private
+            // Protected
+            // Internal
+            // Protected internal
+            // Private protected
+        }
+
+        static void Data_Types()
+        {
+            // Value Type - stack
+            // Reference Type - heap
+        }
+
+        static void Value_Types_Primitive_Types()
+        {
+            // decimal
+            // double
+            // float
+            // long
+            // int
+            // short
+            // char
+            // boolean
+            // byte
+            // Enumerators
+        }
+
+        // enum My_Days_Default_0 { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
+        // enum My_Days_Default_Change_1 { Sun = 1, Mon, Tue, Wed, Thu, Fri, Sat };
+        enum My_Days_as_Short : short { Sun = 1, Mon, Tue, Wed, Thu, Fri, Sat };
+
+        static void Value_Type_Enum_Types()
+        {
+            foreach (My_Days_as_Short day in Enum.GetValues(typeof(My_Days_as_Short)))
+            {
+                Console.WriteLine($"{(int)day}. {day}");
+            }
+        }
+
+        static void Struct_Types()
+        {
+
         }
     }
 }
