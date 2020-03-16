22c270e97c1d2dcd49c6263c8073054cc225d84a
Ray Bishun <raybi@X1EXTREME.bitsbyteslab.local>
2020-03-15 20:45:49 -0400

    Added MvcSearchViaLinq

---
+++ cs/web/mvc/MvcSearchViaLinq/MvcSearchViaLinq/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using MvcSearchViaLinq.Models;
+
+namespace MvcSearchViaLinq.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerDbContext db = new CustomerDbContext();
+        
+        // GET: Customer
+        public ActionResult Index(string name)
+        {
+            var customer = from c in db.Customers
+                           where c.Name == name
+                           select c;
+
+            // var customer = db.Customers.Where(c => c.Name == name);
+
+            return View(customer.ToList());
+        }
+    }
+}
