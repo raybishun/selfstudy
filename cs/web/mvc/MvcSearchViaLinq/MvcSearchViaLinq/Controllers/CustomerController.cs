20350957dcd059e092b0e307d64806a466b33753
Ray Bishun <raybi@X1EXTREME.bitsbyteslab.local>
2020-03-16 12:21:31 -0400

    Upaded MvcSearchViaLinq

--- cs/web/mvc/MvcSearchViaLinq/MvcSearchViaLinq/Controllers/CustomerController.cs
+++ cs/web/mvc/MvcSearchViaLinq/MvcSearchViaLinq/Controllers/CustomerController.cs
@@ -7,8 +7,51 @@ namespace MvcSearchViaLinq.Controllers
     public class CustomerController : Controller
     {
         // GET: Customer
-        public ActionResult Index(string name)
+        public ActionResult Index()
         {
+            // Uses a (default List) View to return all
+
+            using (var db = new CustomerDbContext())
+            {
+                var customers = from c in db.Customers
+                               select c;
+
+                return View(customers.ToList());
+            }
+        }
+
+        public ActionResult List(string name)
+        {
+            // Uses a custom List View to return 1 record
+
+            using (var db = new CustomerDbContext())
+            {
+                var customer = from c in db.Customers
+                               where c.Name == name
+                               select c;
+
+                // var customer = db.Customers.Where(c => c.Name == name);
+
+                return View(customer.ToList());
+            }
+        }
+
+        public ActionResult Details(string name)
+        {
+            using (var db = new CustomerDbContext())
+            {
+                //var customer = from c in db.Customers
+                //               where c.Name == name
+                //               select c;
+
+                return View(db.Customers.Where(x => x.Name == "five").FirstOrDefault());
+            }
+        }
+
+        public ActionResult Details2(string name)
+        {
+            // Uses a custom List View to return 1 record
+
             using (var db = new CustomerDbContext())
             {
                 var customer = from c in db.Customers
