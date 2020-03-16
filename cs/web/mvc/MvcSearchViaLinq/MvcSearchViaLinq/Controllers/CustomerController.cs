0e4f6644bda12bf56d92c92848dae2c8f0b69d63
Ray Bishun <raybi@X1EXTREME.bitsbyteslab.local>
2020-03-15 20:52:12 -0400

    Updated MvcSearchViaLinq

--- cs/web/mvc/MvcSearchViaLinq/MvcSearchViaLinq/Controllers/CustomerController.cs
+++ cs/web/mvc/MvcSearchViaLinq/MvcSearchViaLinq/Controllers/CustomerController.cs
@@ -1,27 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MvcSearchViaLinq.Models;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-
-using MvcSearchViaLinq.Models;
 
 namespace MvcSearchViaLinq.Controllers
 {
     public class CustomerController : Controller
     {
-        CustomerDbContext db = new CustomerDbContext();
-        
         // GET: Customer
         public ActionResult Index(string name)
         {
-            var customer = from c in db.Customers
-                           where c.Name == name
-                           select c;
+            using (var db = new CustomerDbContext())
+            {
+                var customer = from c in db.Customers
+                               where c.Name == name
+                               select c;
 
-            // var customer = db.Customers.Where(c => c.Name == name);
+                // var customer = db.Customers.Where(c => c.Name == name);
 
-            return View(customer.ToList());
+                return View(customer.ToList());
+            }
         }
     }
 }
