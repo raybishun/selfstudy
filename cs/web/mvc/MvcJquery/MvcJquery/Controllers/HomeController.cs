a90403d0bb66aeaf129c7a497d07ced07751f2e3
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-03-19 21:40:46 -0400

    Updated MvcJquery and MvcSearchViaLinq

---
+++ cs/web/mvc/MvcJquery/MvcJquery/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using MvcJquery.Models;
+
+namespace MvcJquery.Controllers
+{
+    public class HomeController : Controller
+    {
+        MvcJqueryEntities db = new MvcJqueryEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetStudentList()
+        {
+            List<StudentViewModel> StuList = db.tblStudents.Where(x => x.IsDeleted == false).Select(x => new StudentViewModel
+            {
+                StudentId = x.StudentId,
+                StudentName = x.StudentName,
+                Email = x.Email,
+                DepartmentName = x.tblDepartment.DepartmentName
+            }).ToList();
+
+            return Json(StuList, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
