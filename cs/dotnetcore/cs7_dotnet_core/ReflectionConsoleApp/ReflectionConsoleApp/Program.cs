9cc52fb75e5336c6778b1c7b791e56a825b6b17f
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-03-21 20:10:32 -0400

    Added ReflectionConsoleApp

---
+++ cs/dotnetcore/cs7_dotnet_core/ReflectionConsoleApp/ReflectionConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ReflectionConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Xml.XmlReader reader;
+            System.Xml.Linq.XElement element;
+            System.Net.Http.HttpClient client;
+            
+            // loop through the assemblies that this Console App references
+            foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                // load the assembly to read its details
+                var a = Assembly.Load(new AssemblyName(r.Name));
+
+                int methodCount = 0;
+
+                // loop through all types in the assembly
+                foreach (var t in a.DefinedTypes)
+                {
+                    methodCount += t.GetMethods().Count();
+                }
+
+                Console.WriteLine($"{a.DefinedTypes.Count():N0} types " +
+                    $"with {methodCount:N0} methods in {r.Name} assembly.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
