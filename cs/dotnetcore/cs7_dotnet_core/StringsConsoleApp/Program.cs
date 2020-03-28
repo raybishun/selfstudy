c9d619683a3a232f8d0534a08bb8d02535960351
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-03-28 15:05:10 -0400

    Updated String Methods

--- cs/dotnetcore/cs7_dotnet_core/StringsConsoleApp/Program.cs
+++ cs/dotnetcore/cs7_dotnet_core/StringsConsoleApp/Program.cs
@@ -10,9 +10,57 @@ namespace StringsConsoleApp
             // String_Split();
             // String_IndexOf_and_Substring();
             // String_Contains();
-            String_StartsWith();
+            // String_StartsWith();
+            // String_Replace();
+            // String_Concat();
+            // String_Join();
+            String_Empty();
 
             Console.ReadLine();
+        }
+
+        private static void String_Empty()
+        {
+            string value = "";
+
+            if (string.IsNullOrEmpty(value))
+            {
+                Console.WriteLine("The string is empty, \"\" or null.");
+            }
+
+
+            string value2 = string.Empty;
+
+            if (string.IsNullOrEmpty(value2))
+            {
+                Console.WriteLine("*** NOTE: string.Empty DOES NOT ALLOCATE MEMORY - Use this instead of string value = \"\" ***");
+            }
+        }
+
+        private static void String_Join()
+        {
+            string firstName = "ray";
+            string lastName = "bishun";
+
+            string fullName = string.Join(' ', firstName, lastName);
+            Console.WriteLine(fullName);
+        }
+
+        private static void String_Concat()
+        {
+            string firstName = "ray ";
+            string lastName = "bishun";
+
+            string fullName = string.Concat(firstName, lastName);
+            Console.WriteLine(fullName);
+        }
+
+        private static void String_Replace()
+        {
+            string fullName = "ray bishun";
+            string newName = fullName.Replace("ray", "roy");
+            Console.WriteLine(newName);
+
         }
 
         private static void String_StartsWith()
