b5174342551387cc00d58099ce79f41792fdd1c9
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-03-28 12:01:38 -0400

    Added String Methods

---
+++ cs/dotnetcore/cs7_dotnet_core/StringsConsoleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace StringsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // String_Char_Array();
+            // String_Split();
+            // String_IndexOf_and_Substring();
+            // String_Contains();
+            String_StartsWith();
+
+            Console.ReadLine();
+        }
+
+        private static void String_StartsWith()
+        {
+            string fullName = "ray bishun";
+            bool find = fullName.StartsWith('R'); // returns False, is case sensitive
+            Console.WriteLine(find);
+        }
+
+        private static void String_Contains()
+        {
+            string fullName = "ray bishun";
+            bool find = fullName.Contains('a');
+            Console.WriteLine(find);
+        }
+
+        private static void String_IndexOf_and_Substring()
+        {
+            string fullName = "ray bishun";
+            int indexOfTheSpace = fullName.IndexOf(' ');
+            string firstName = fullName.Substring(0, indexOfTheSpace);
+            string lastName = fullName.Substring(indexOfTheSpace);
+            Console.WriteLine($"{lastName}, {firstName}");
+        }
+
+        private static void String_Split()
+        {
+            string fullName = "ray bishun";
+            string[] elements = fullName.Split(' ');
+            Console.WriteLine(elements[0]);
+        }
+
+        private static void String_Char_Array()
+        {
+            string name = "ray";
+            Console.WriteLine(name[0]);
+        }
+    }
+}
