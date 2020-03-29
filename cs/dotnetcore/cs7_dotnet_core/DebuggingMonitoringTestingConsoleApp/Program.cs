c555b0e7c86b60ca40382e0c784eb9fd9e500d91
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-03-29 14:30:22 -0400

    Added DebuggingMonitoringTestingConsoleApp

---
+++ cs/dotnetcore/cs7_dotnet_core/DebuggingMonitoringTestingConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace DebuggingMonitoringTestingConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Monitoring_Stopwatch();
+
+            Console.ReadLine();
+        }
+
+        private static void Monitoring_Stopwatch()
+        {
+            Console.WriteLine("Press ENTER to start timer: ");
+            Console.ReadLine();
+
+            Recorder.Start();
+
+            int[] largeArrayOfInts = Enumerable.Range(1, 10000).ToArray();
+
+            Console.WriteLine("Press ENTER to stop timer: ");
+            Console.ReadLine();
+
+            Recorder.Stop();
+        }
+    }
+}
---
+++ cs/dotnetcore/cs7_dotnet_core/DebuggingMonitoringTestingConsoleApp/Recorder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DebuggingMonitoringTestingConsoleApp
+{
+    class Recorder
+    {
+        static Stopwatch timer = new Stopwatch();
+        static long bytesPhysicalBefore = 0;
+        static long bytesVirtualBefore = 0;
+
+        public static void Start()
+        {
+            // Force an immediate GC on all generations
+            GC.Collect();
+
+            // Suspend the current thread 
+            // until the thread that is processing the queue of finalizers 
+            // has emptied that queue
+            // NOTE: DO NOT USE IN PRODUCTION CODE !!!
+            GC.WaitForPendingFinalizers();
+
+            // Force an immediate GC on all generations, again (essentially flushing out GC)
+            GC.Collect();
+
+            // Gets the amount of physical memory, in bytes, allocated for the associated process
+            bytesPhysicalBefore = Process.GetCurrentProcess().WorkingSet64;
+
+            // Gets the amount of virtual memory, in bytes, allocated for the associated process
+            bytesVirtualBefore = Process.GetCurrentProcess().VirtualMemorySize64;
+
+            // Reset time to 0 and start the stopwatch
+            timer.Restart();
+        }
+
+        public static void Stop()
+        {
+            timer.Stop();
+            long bytesPhysicalAfter = Process.GetCurrentProcess().WorkingSet64;
+            long bytesVirtualAfter = Process.GetCurrentProcess().VirtualMemorySize64;
+
+            Console.WriteLine("Stopped recording.");
+            Console.WriteLine($"{bytesPhysicalAfter - bytesPhysicalBefore:N0} physical bytes used.");
+            Console.WriteLine($"{bytesVirtualAfter - bytesVirtualBefore:N0} virtual bytes used.");
+
+            // The Elapsed property returns a TimeSpan (the total timespan elapsed in hours:minutes:seconds)
+            Console.WriteLine($"{timer.Elapsed} total timespan elapsed.");
+
+            // The ElapsedMilliseconds property returns a long (the total timespan elapsed in ms) 
+            Console.WriteLine($"{timer.ElapsedMilliseconds:N0} total milliseconds elapsed.");
+        }
+    }
+}
