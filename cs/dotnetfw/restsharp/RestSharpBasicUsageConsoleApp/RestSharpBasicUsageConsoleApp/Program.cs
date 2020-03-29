0721baa35ba2c80491091280a809c3d67493d69b
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-03-28 23:33:27 -0400

    Added RestSharpBasicUsageConsoleApp

---
+++ cs/dotnetfw/restsharp/RestSharpBasicUsageConsoleApp/RestSharpBasicUsageConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace RestSharpBasicUsageConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // BasicUsage_Twitter();
+            BasicUsage_ReqRes();
+
+
+            Console.ReadLine();
+        }
+
+        private static void BasicUsage_ReqRes()
+        {
+            var client = new RestClient("https://reqres.in/");
+
+            var request = new RestRequest("/api/users/2", DataFormat.Json);
+
+            var response = client.Get(request);
+
+            Console.WriteLine(response.Content);
+        }
+
+        static void BasicUsage_Twitter()
+        {
+            try
+            {
+                var client = new RestClient("https://api.twitter.com/1.1");
+                client.Authenticator = new HttpBasicAuthenticator("username", "password");
+
+                var request = new RestRequest("statuses/home_timeline.json", DataFormat.Json);
+
+                var response = client.Get(request);
+
+                Console.WriteLine(response.StatusDescription);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
