c158e3a0532e192d1095cd636287aaea763ec864
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-03-28 20:27:19 -0400

    Added GlobalizationConsoleApp

---
+++ cs/dotnetcore/cs7_dotnet_core/GlobalizationConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace GlobalizationConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo globalization = CultureInfo.CurrentCulture;
+            CultureInfo localization = CultureInfo.CurrentUICulture;
+            Console.WriteLine(globalization.Name);
+            Console.WriteLine(localization.Name);
+
+            Console.ReadLine();  
+        }
+    }
+}
