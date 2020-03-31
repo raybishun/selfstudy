d48568e539edd610b0c8595380f827e80e712d6a
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-03-31 19:54:38 -0400

    RestSharpBasicUsage2UnitTest

--- cs/dotnetfw/restsharp/RestSharpBasicUsage2UnitTest/RestSharpBasicUsage2UnitTest/UnitTest1.cs
+++ cs/dotnetfw/restsharp/RestSharpBasicUsage2UnitTest/RestSharpBasicUsage2UnitTest/UnitTest1.cs
@@ -1,14 +1,38 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Serialization.Json;
 
 namespace RestSharpBasicUsage2UnitTest
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
+        [Test]
         public void TestMethod1()
         {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("posts/{postid}", Method.GET);
+            request.AddUrlSegment("postid", 1);
+
+            //var content = client.Execute(request).Content;
+            //Console.WriteLine(content);
+
+            var response = client.Execute(request);
+
+            //var deserialize = new JsonDeserializer();
+            //var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            //var result = output["author"];
+            //Assert.That(result, Is.EqualTo("Diana Prince"), "Author is invalid.");
+
+            // using Newtonsoft (another option)
+            JObject obs = JObject.Parse(response.Content);
+            Assert.That(obs["author"].ToString(), Is.EqualTo("Diana Prince"), "Author is invalid.");
+
+       
         }
     }
 }
