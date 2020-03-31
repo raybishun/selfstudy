3068430c544de60bb4d2513e305b860b35025227
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-03-30 20:59:09 -0400

    Created RestSharpBasicUsage2UnitTest

---
+++ cs/dotnetfw/restsharp/RestSharpBasicUsage2UnitTest/RestSharpBasicUsage2UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestSharpBasicUsage2UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestSharpBasicUsage2UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7d28a101-4050-46f9-8f19-cc9a19ad4c2e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
