c76f7e986ac60b32081fa780772fe93076cb056d
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-04-03 23:40:48 -0400

    Working with HttpResponseMessage's Status Codes

--- cs/dotnetfw/restsharp/WebServiceAutomation/WebServiceAutomation/GetEndpoint/TestGetEndpoint.cs
+++ cs/dotnetfw/restsharp/WebServiceAutomation/WebServiceAutomation/GetEndpoint/TestGetEndpoint.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,7 +31,58 @@ namespace WebServiceAutomation.GetEndpoint
 
             Uri getUri = new Uri(getURL);
 
-            httpClient.GetAsync(getUri);
+            // httpClient.GetAsync(getUri);
+
+            Task<HttpResponseMessage> httpResponse = httpClient.GetAsync(getUri);
+
+            HttpResponseMessage httpResponseMessage = httpResponse.Result;
+            
+            Console.WriteLine(httpResponseMessage.ToString());
+
+            HttpStatusCode statusCode = httpResponseMessage.StatusCode;
+
+            Console.WriteLine($"Status Code: {statusCode}");
+            Console.WriteLine($"Status Code: {(int)statusCode}");
+
+            HttpContent responseContent = httpResponseMessage.Content;
+
+            Task<string> responseData = responseContent.ReadAsStringAsync();
+
+            string data = responseData.Result;
+
+            Console.WriteLine($"{data}");
+
+            httpClient.Dispose();
+        }
+
+        [TestMethod]
+        public void TestGetAllEndpointWithInvalidUrl()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            // Modify URL to return a NonFound 404 HTTP error
+            Uri getUri = new Uri($"{getURL}_Invalid_Url_Here");
+
+            Task<HttpResponseMessage> httpResponse = httpClient.GetAsync(getUri);
+
+            HttpResponseMessage httpResponseMessage = httpResponse.Result;
+
+            Console.WriteLine(httpResponseMessage.ToString());
+
+            HttpStatusCode statusCode = httpResponseMessage.StatusCode;
+
+            Console.WriteLine($"Status Code: {statusCode}");
+            Console.WriteLine($"Status Code: {(int)statusCode}");
+
+            HttpContent responseContent = httpResponseMessage.Content;
+
+            Task<string> responseData = responseContent.ReadAsStringAsync();
+
+            string data = responseData.Result;
+
+            Console.WriteLine($"{data}");
+
+
 
             httpClient.Dispose();
         }
