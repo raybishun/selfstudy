f148e7f0a5941b83858bb99ec7b034e9b0387a32
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-04-16 08:39:05 -0400

    Added RestClientHelper class

---
+++ cs/dotnetfw/restsharp/WebServiceAutomation/RestSharpAutomation/Helpers/Request/RestClientHelper.cs
@@ -0,0 +1,54 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpAutomation.HelperClass.Request
+{
+    public class RestClientHelper
+    {
+        private IRestClient GetRestClient()
+        {
+            IRestClient restClient = new RestClient();
+            return restClient;
+        }
+
+        private IRestRequest GetRequest(
+            string url, Dictionary<string, string> header, Method method)
+        {
+            IRestRequest restRequest = new RestRequest()
+            {
+                Resource = url,
+                Method = method
+            };
+
+            foreach (var item in header)
+            {
+                restRequest.AddHeader(item.Key, item.Value);
+            }
+
+            //foreach (string key in header.Keys)
+            //{
+            //    restRequest.AddHeader(key, header[key]);
+            //}
+
+            return restRequest;
+        }
+
+        private IRestResponse SendRequest(IRestRequest restRequest)
+        {
+            IRestClient restClient = GetRestClient();
+            IRestResponse restResponse = restClient.Execute(restRequest);
+            return restResponse;
+        }
+
+        private IRestResponse<T> SendRequest<T>(IRestRequest restRequest) where T : new()
+        {
+            IRestClient restClient = GetRestClient();
+            IRestResponse<T> restResponse = restClient.Execute<T>(restRequest);
+            return restResponse;
+        }
+    }
+}
