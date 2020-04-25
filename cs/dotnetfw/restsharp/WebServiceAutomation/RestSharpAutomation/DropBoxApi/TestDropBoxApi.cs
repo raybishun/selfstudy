1ad0df290d007750360d30143e7a2a24cbd2cde6
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-04-24 23:17:14 -0400

    Added TestDropBoxAPI.TestFileDownload()

--- cs/dotnetfw/restsharp/WebServiceAutomation/RestSharpAutomation/DropBoxApi/TestDropBoxApi.cs
+++ cs/dotnetfw/restsharp/WebServiceAutomation/RestSharpAutomation/DropBoxApi/TestDropBoxApi.cs
@@ -3,6 +3,7 @@ using RestSharp;
 using RestSharpAutomation.DropBoxAPI.ListFolderModel;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ namespace RestSharpAutomation.DropBoxAPI
         private static string accessToken = creds[0];
         private static string listFolderUrl = "https://api.dropboxapi.com/2/files/list_folder";
         private static string createFolderUrl = "https://api.dropboxapi.com/2/files/create_folder_v2";
+        private static string downloadUrl = "https://content.dropboxapi.com/2/files/download";
 
         [TestMethod]
         public void TestListFolder()
@@ -50,10 +52,27 @@ namespace RestSharpAutomation.DropBoxAPI
             request.RequestFormat = DataFormat.Json;
             request.AddBody(body);
 
-            IRestResponse response = client.Post<RootObject>(request);
+            IRestResponse response = client.Post(request);
             
             Assert.AreEqual(200, (int)response.StatusCode);
         }
 
+        [TestMethod]
+        public void TestFileDownload()
+        {
+            string srcFile = "{\"path\": \"/Book.xlsx\"}";
+            string dstFile = "Test.xlsx";
+            
+            IRestClient client = new RestClient();
+
+            IRestRequest request = new RestRequest() { Resource = downloadUrl };
+            request.AddHeader("Authorization", $"Bearer {accessToken}");
+            request.AddHeader("Dropbox-API-Arg", srcFile);
+            request.RequestFormat = DataFormat.Json;
+           
+            byte[] byteArrayResponse = client.DownloadData(request);
+
+            File.WriteAllBytes(dstFile, byteArrayResponse);
+        }
     }
 }
