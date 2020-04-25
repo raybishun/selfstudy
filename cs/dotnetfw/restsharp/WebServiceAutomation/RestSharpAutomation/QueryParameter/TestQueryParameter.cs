170f53801982cce9adf214bd84f6ffea8a27f3d7
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-04-25 19:41:34 -0400

    Added TestQueryParameter.TestQueryString()

---
+++ cs/dotnetfw/restsharp/WebServiceAutomation/RestSharpAutomation/QueryParameter/TestQueryParameter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System.Net;
+using WebServiceAutomation.Model.XmlModel;
+
+namespace RestSharpAutomation.QueryParameter
+{
+    [TestClass]
+    public class TestQueryParameter
+    {
+        private readonly string searchUrl = "http://localhost:8080/laptop-bag/webapi/api/query";
+        
+        [TestMethod]
+        public void TestQueryString()
+        {
+            IRestClient client = new RestClient();
+
+            IRestRequest request = new RestRequest() { Resource = searchUrl };
+            request.AddHeader("Accept", "application/xml");
+
+            // Option #1
+            // request.AddParameter("id", "1", ParameterType.QueryString);
+
+            // Option #2
+            request.AddQueryParameter("id", "1");
+            request.AddQueryParameter("laptopName", "Alienware M17");
+
+            IRestResponse<Laptop> response = client.Get<Laptop>(request);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.AreEqual("Alienware", response.Data.BrandName);
+        }
+    }
+}
