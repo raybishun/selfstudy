6ebbd0a1a5a57cdafe4d05e393b29957fda58bc4
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-05-05 21:05:36 -0400

    Added ADODotNetRead

---
+++ cs/foundation/ProgrammingInCS/ADODotNetRead/Animal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADODotNetRead
+{
+    class Animal
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ cs/foundation/ProgrammingInCS/ADODotNetRead/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace ADODotNetRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // http://msdn.microsoft.com/en-us/library/system.data.sqlclient.sqlconnection.open.aspx 
+            // http://msdn.microsoft.com/en-us/library/system.data.sqlclient.sqldatareader.read.aspx   
+        }
+
+        private static IEnumerable<Animal> GetAnimals(string sqlConStr)
+        {
+            var animals = new List<Animal>();
+
+            SqlConnection sqlCon = new SqlConnection(sqlConStr);
+
+            sqlCon.Open();
+
+            using (sqlCon)
+            {
+                SqlCommand sqlCmd = new SqlCommand("SELECT Name, Color FROM Animals", sqlCon);
+
+                using (SqlDataReader sqlreader = sqlCmd.ExecuteReader())
+                {
+                    while (sqlreader.Read())
+                    {
+                        var animal = new Animal();
+
+                        animal.Name = (string)sqlreader["Name"];
+                        animal.Color = (string)sqlreader["Color"];
+
+                        animals.Add(animal);
+                    }
+                }
+            }
+
+            return animals;
+        }
+    }
+}
