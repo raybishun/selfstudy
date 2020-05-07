a985823657874c3d7f994dff14b3cda186082138
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-05-07 19:46:55 -0400

    Added CollectionsIEnumerable2 project

---
+++ cs/foundation/ProgrammingInCS/CollectionsIEnumerable2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CollectionsIEnumerable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Basket basket = new Basket(
+                new string[] { "Apple", "Bananas", "Blueberry" });
+
+            foreach (string fruit in basket)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+
+    class Basket : IEnumerable<string>
+    {
+        readonly List<string> items;
+
+        public Basket(string[] array)
+        {
+            items = new List<string>(array);
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            Console.WriteLine("waiting for a foreach to implicitly call me...");
+            return items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+    }
+}
