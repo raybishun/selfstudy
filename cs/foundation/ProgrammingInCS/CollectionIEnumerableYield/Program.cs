11939056222ecbce57550cdb9c6d9b2492b0f6d9
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-05-07 19:10:19 -0400

    Added CollectionIEnumerableYield project

---
+++ cs/foundation/ProgrammingInCS/CollectionIEnumerableYield/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace CollectionIEnumerableYield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int i in GetNumbers(0, 10))
+            {
+                if (i == 3)
+                {
+                    break;
+                }
+
+                Console.WriteLine(i);
+            }
+        }
+
+        static IEnumerable GetNumbers(int start, int end)
+        {
+            for (; start <= end; start++)
+            {
+                yield return start;
+            }
+        }
+    }
+}
