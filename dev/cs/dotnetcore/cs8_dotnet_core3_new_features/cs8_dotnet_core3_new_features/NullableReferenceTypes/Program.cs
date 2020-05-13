51b457adc38116949161d5a470372177667153a7
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-05-13 19:53:19 -0400

    Added NullableReferenceTypes Project

---
+++ dev/cs/dotnetcore/cs8_dotnet_core3_new_features/cs8_dotnet_core3_new_features/NullableReferenceTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NullableReferenceTypes
+{
+    class Program
+    { static void Main(string[] args)
+        {
+            var student = new Student() { FirstName = "Ray" };
+
+            Console.WriteLine($"{student.LastName.ToUpper()}, {student.FirstName.ToUpper()}");
+        }
+    }
+}
