65af0b4152dfac9275c1adcaf53448491544a8aa
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-05-13 21:41:18 -0400

    Added Ranges

---
+++ dev/cs/dotnetcore/cs8_dotnet_core3_new_features/cs8_dotnet_core3_new_features/Ranges/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace Ranges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RangeDemo();
+        }
+
+        static void RangeDemo()
+        {
+            string[] letters = new string[]
+            { 
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+                "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+                "U", "V", "W", "X", "Y", "Z"
+            };
+
+            StringBuilder sb = new StringBuilder();
+            
+            Range range = 1..5;
+
+            Range range1 = 10..^2;
+
+            Range range2 = 10..;
+
+            foreach (var item in letters[range2])
+            {
+                sb.Append($"{item} ");
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
