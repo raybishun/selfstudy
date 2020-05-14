7ac8dc296995486c5c25dc6a8b7728af666d67d5
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-05-13 21:15:15 -0400

    Added AsyncStreams Project

---
+++ dev/cs/dotnetcore/cs8_dotnet_core3_new_features/cs8_dotnet_core3_new_features/AysncStreams/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AysncStreams
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            await foreach (var student in GetStudentsAsync())
+            {
+                Console.WriteLine($"{student.FirstName}");
+            }        
+        }
+
+        async static IAsyncEnumerable<Student> GetStudentsAsync()
+        {
+            var list = new List<Student>()
+            {
+                new Student() { FirstName = "Peter", LastName = "Parker", Email = "peter.parker@gmail.com", Gpa = 3.9},
+                new Student() { FirstName = "Bruce", LastName = "Wayne", Email = "bruce.wayne@gmail.com", Gpa = 4.1},
+                new Student() { FirstName = "Clark", LastName = "Kent", Email = "clark.kent@gmail.com", Gpa = 4.0}
+            };
+
+            foreach (var student in list)
+            {
+                await Task.Delay(2000);
+
+                // yield returns something asynchronously
+                // In this case, yield, will return student as an asynchronous stream
+                yield return student;
+            }
+        }
+    }
+}
--- dev/cs/dotnetcore/cs8_dotnet_core3_new_features/cs8_dotnet_core3_new_features/AysncStreams/Student.cs
+++ dev/cs/dotnetcore/cs8_dotnet_core3_new_features/cs8_dotnet_core3_new_features/AysncStreams/Student.cs
@@ -1,4 +1,4 @@
-﻿namespace NullableReferenceTypes
+﻿namespace AysncStreams
 {
     class Student
     {
