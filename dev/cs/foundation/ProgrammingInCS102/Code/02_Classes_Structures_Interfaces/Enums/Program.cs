e2d4e4c7f12ecf56626b93837102faa9f6933f9f
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-05-17 15:39:26 -0400

    Added Enums Project

---
+++ dev/cs/foundation/ProgrammingInCS102/Code/02_Classes_Structures_Interfaces/Enums/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var midWeek = WorkWeek.Wed;
+            Console.WriteLine(midWeek);
+        }
+    }
+}
---
+++ dev/cs/foundation/ProgrammingInCS102/Code/02_Classes_Structures_Interfaces/Enums/WorkWeek.cs
@@ -0,0 +1,11 @@
+﻿namespace Enums
+{
+    enum WorkWeek : short
+    {
+        Mon = 1,
+        Tue = 2,
+        Wed = 3,
+        Thu = 4,
+        Fri = 5
+    }
+}
