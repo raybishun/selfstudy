c28ada24585f0064c3ba7ca10e712cfe198c7304
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-05-21 18:35:16 -0400

    Proved structs are able to use Interfaces

--- dev/cs/foundation/ProgrammingInCS102/Code/02_Classes_Structures_Interfaces/StructsVsClasses/CoordinatePointStruct.cs
+++ dev/cs/foundation/ProgrammingInCS102/Code/02_Classes_Structures_Interfaces/StructsVsClasses/CoordinatePointStruct.cs
@@ -1,19 +1,29 @@
 ﻿namespace StructsVsClasses
 {
-    struct CoordinatePointStruct
+    struct CoordinatePointStruct : ITest
     {
         public float X { get; set; }
         public float Y { get; set; }
+        
 
         //public CoordinatePointStruct()
         //{
         //    // Structs cannot contain explicit parameterless ctors
         //}
-        
+
         public CoordinatePointStruct(float x, float y)
         {
             X = x;
             Y = y;
+        }
+
+
+
+        // Yes, a struct can use interfaces...
+        public int SomeInt { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string MustDowork(string[] arrayOfWork)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
---
+++ dev/cs/foundation/ProgrammingInCS102/Code/02_Classes_Structures_Interfaces/StructsVsClasses/ITest.cs
@@ -0,0 +1,14 @@
+﻿namespace StructsVsClasses
+{
+    interface ITest
+    {
+        // Interfaces cannot contains instance fields
+        // public int SomeInt;
+
+        // but Interfaces can contain properties
+        int SomeInt { get; set; }
+
+        // and of course, method signatures
+        string MustDowork(string[] arrayOfWork);
+    }
+}
