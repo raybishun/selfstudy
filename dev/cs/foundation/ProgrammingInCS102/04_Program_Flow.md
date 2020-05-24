cc40dc6e930e9bf1ff3547caba44e0d353c8f72c
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-05-24 11:50:23 -0400

    Added 04_Program_Flow Solution

---
+++ dev/cs/foundation/ProgrammingInCS102/04_Program_Flow.md
@@ -0,0 +1,37 @@
+# Understanding Operators
+
+### Operators
+- Are used to evaluate/manipulate one or more operands in an expression or statement
+
+### Operands
+- The object(s) being evaluated/manipulated
+
+### Types of Operators
+- Unary (1 operand)
+- Binary (2 operands)
+- Ternary (3 operands)
+- Relational
+- Equality
+- Shift
+
+### Unary Operators
+- +x (Identity)
+- -x (Negation)
+- !x (Logical negation)
+- ~x (Bitwise negation)
+- ++x (Pre-increment)
+- --x (Pre-decrement)
+
+### Relational Operators
+- <
+- >
+- <=
+- >=
+
+### Equality Operators
+- == is equal to
+- != is not equal to
+
+### Shift Operators
+- <<
+- >>
---
+++ dev/cs/foundation/ProgrammingInCS102/Code/04_ProgramFlow/OperatorsBitwiseNegation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OperatorsBitwiseNegation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 3;
+            
+            string binaryString = IntToBinaryString(myInt);
+            Console.WriteLine(binaryString);
+
+            // Reverse each bit
+            int bitwiseNegation = ~myInt;
+            Console.WriteLine(bitwiseNegation);
+        }
+
+        static string IntToBinaryString(int value)
+        {
+            // Convert value of a 32-bit signed integer to it equivalent string
+            // representation in a specified base (2, 8, 10, or 16)
+            return Convert.ToString(value, 2);
+        }
+    }
+}
