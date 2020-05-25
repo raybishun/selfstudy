d51e0a87bfb7e51ecb49af6ac7f934a16c323841
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-05-24 21:45:14 -0400

    Added ConditionalStatements Project

---
+++ dev/cs/foundation/ProgrammingInCS102/Code/04_ProgramFlow/ConditionalStatements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Switch_Example();
+        }
+
+        static void Switch_Example()
+        {
+            string input;
+
+            do
+            {
+                Console.Write("Enter a character: ");
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "a":
+                    case "e":
+                    case "i":
+                    case "o":
+                    case "u":
+                        Console.WriteLine($"\tThank you for vowel: {input}");
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (input != "x");
+
+            
+        }
+    }
+}
