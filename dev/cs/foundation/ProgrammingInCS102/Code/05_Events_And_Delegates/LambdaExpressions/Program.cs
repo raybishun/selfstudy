858b8844c1e60cdeff53dfcaceb6a5e74b5718b1
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-05-26 21:30:46 -0400

    Add two search methods to LambdaExpressions Project

--- dev/cs/foundation/ProgrammingInCS102/Code/05_Events_And_Delegates/LambdaExpressions/Program.cs
+++ dev/cs/foundation/ProgrammingInCS102/Code/05_Events_And_Delegates/LambdaExpressions/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Linq;
 
 namespace LambdaExpressions
 {
     class Program
     {
+        static string[] faanam = { "FaceBook", "Amazon", "Apple", "Netflix", "Alphabet", "Microsoft" };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Search1(faanam, "Microsoft"));
+            Console.WriteLine(Search2(faanam, "Microsoft"));
+        }
+
+        static string Search1(string[] array, string element)
+        {
+            return array.Where(item => item.Equals(element.Trim())).FirstOrDefault();
+        }
+
+        static string Search2(string[] array, string element)
+        {
+            var query = from item in array
+                        where item.Equals(element.Trim())
+                        select item;
+
+            return query.FirstOrDefault();
         }
     }
 }
