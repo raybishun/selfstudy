c8f2431a217fe093b2859d841070f76b39d1e562
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-06-06 11:34:50 -0400

    Modified dotnet_core_3 hellow_world

--- dev/cs/dotnetcore/intro_to_dotnet_core_3/code/hello_world/Startup.cs
+++ dev/cs/dotnetcore/intro_to_dotnet_core_3/code/hello_world/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace hello_world
@@ -44,7 +46,16 @@ namespace hello_world
 
             app.UseHttpsRedirection();
 
+            // Middleware
             app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(),
+                        "node_modules")
+                    ),
+                    RequestPath = "/3rdParty"
+            });
 
             app.UseRouting();
 
