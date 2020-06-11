91c846c5468bbf1b792b094581d913277e3b06ce
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-06-10 21:56:08 -0400

    Ran add-migration, update-database

---
+++ dev/languages/cs/dotnetcore/intro_to_dotnet_core_3_1/razor/BooksRazor/BooksRazor/Model/Book.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksRazor.Model
+{
+    // Dependencies (packages)
+    // -----------------------
+    // EFCore
+    // EFCore.SqlServer
+    // EFCore.Tools (for migrations)
+
+    // To push the model to SQL Server and create the database, perform:
+    // Tools\NuGet Package Manager\Package Manager Console
+    // PM> add-migration AddBooktoDb
+        /* The add-migration CMD creates the following script:
+     *
+        public partial class AddBooktoDb : Migration
+        {
+            protected override void Up(MigrationBuilder migrationBuilder)
+            {
+                migrationBuilder.CreateTable(
+                    name: "Book",
+                    columns: table => new
+                    {
+                        Id = table.Column<int>(nullable: false)
+                            .Annotation("SqlServer:Identity", "1, 1"),
+                        Name = table.Column<string>(nullable: false),
+                        Author = table.Column<string>(nullable: true)
+                    },
+                    constraints: table =>
+                    {
+                        table.PrimaryKey("PK_Book", x => x.Id);
+                    });
+            }
+
+            protected override void Down(MigrationBuilder migrationBuilder)
+            {
+                migrationBuilder.DropTable(
+                    name: "Book");
+            }
+        }
+     */
+    // PM> update-database (to execute the above script)
+
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
