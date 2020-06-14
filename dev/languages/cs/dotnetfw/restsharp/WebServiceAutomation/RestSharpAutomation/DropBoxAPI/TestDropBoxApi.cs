841c01a40eea0718bbce2983a3379830c3175a7b
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-06-14 19:04:39 -0400

    Modified DropBoxModel Classes

--- dev/languages/cs/dotnetfw/restsharp/WebServiceAutomation/RestSharpAutomation/DropBoxAPI/TestDropBoxApi.cs
+++ dev/languages/cs/dotnetfw/restsharp/WebServiceAutomation/RestSharpAutomation/DropBoxAPI/TestDropBoxApi.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RestSharp;
 using RestSharpAutomation.DropBoxAPI.ListFolderModel;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace RestSharpAutomation.DropBoxAPI
