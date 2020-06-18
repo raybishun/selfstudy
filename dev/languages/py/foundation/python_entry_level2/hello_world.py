e3167c7626bb751986ff3773519bccad67697419
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-06-17 21:24:26 -0400

    Added hello_world.py

---
+++ dev/languages/py/foundation/python_entry_level2/hello_world.py
@@ -0,0 +1,11 @@
+# (Make hello_world.py executable on Linux)
+#!/usr/bin/env python3.8
+
+# (Make hello_world executable for the user) 
+# chmod u+x hello_world.py
+
+# (To execute, simply type the below)
+# (this simply passes the .py file to the python interpreter)
+# ./hello_world.py
+
+print("Hello World, again...")
