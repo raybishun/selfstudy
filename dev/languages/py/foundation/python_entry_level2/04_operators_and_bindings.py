8c32611279f45516631dc5108d04d20a99f43e71
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-06-25 21:04:00 -0400

    Modified 04_operators_and_bindings.py with Bitwise: XOR, and Shift Operators

--- dev/languages/py/foundation/python_entry_level2/04_operators_and_bindings.py
+++ dev/languages/py/foundation/python_entry_level2/04_operators_and_bindings.py
@@ -1,5 +1,6 @@
+# =============================================================================
 # Unary and Bitwise Operators
-
+# =============================================================================
 # Unary operator: only has a right-side operand (in contrast, the + operator takes 2 operands)
 # Bitwise operator: makes use of bits in a Truth Table
 
@@ -63,5 +64,38 @@ print(bin(c))
 print('---\n')
 
 # -----------------------------------------------------------------------------
-# Bitwise: XOR
+# Bitwise: XOR (aka Exclusive OR) 
+#   requires 1, and only 1 to be true
+#   note: 1 and 1 = 0
+#   also: 0 and 0 = 0
 # -----------------------------------------------------------------------------
+a = 0b0101 # returns 5
+b = 0b0100 # returns 4
+#   0b0001
+print(bin(a))   # 5
+print(bin(b))   # 4
+c = a ^ b       # 1
+print(bin(c))
+print('---\n')
+
+# -----------------------------------------------------------------------------
+# Shift Operators: Right-Shift ( >> )
+# -----------------------------------------------------------------------------
+# The 'Right' Shift Operator
+# For the example below, shift 2 places to the right
+a = 0b0110 # returns 6
+print(bin(a))
+print(bin(a >> 2))
+print(bin(a >> 4)) # leading 0 if you attempt to shift too far (returns 0b0)
+print('---\n')
+
+# -----------------------------------------------------------------------------
+# Shift Operators: Left-Shift ( << )
+# -----------------------------------------------------------------------------
+# The 'Left' Shift Operator
+# For the example below, shift 2 places to the left
+# And of course, the number grows (as you're adding 0s to the end)
+a = 0b0110 # returns 6
+print(bin(a))       # 0b110
+print(bin(a << 2))  # 0b11000 (or 24)
+print('---\n')
