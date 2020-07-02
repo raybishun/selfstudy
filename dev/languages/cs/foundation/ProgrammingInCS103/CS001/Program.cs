61191f65ab37cbd2493146244dc3ccacdd14f4ea
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-07-01 21:57:21 -0400

    Added ProgrammingInCS103 Solution: Project CS001

---
+++ dev/languages/cs/foundation/ProgrammingInCS103/CS001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProgrammingInCS103
+{
+    class Program
+    {
+        // Converting a reference type into a value type
+        static void Temp(float degree)
+        {
+            object degreesRef = degree;
+            int result = (int)(float)degreesRef;
+            Console.WriteLine(result);
+        }
+
+        static void Main(string[] args)
+        {
+            Temp(32.3f);
+            Console.ReadKey();
+        }
+    }
+}
