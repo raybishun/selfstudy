b8a74912ba20e6130c6b2bdbf118a54ba80490fd
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-07-04 16:38:10 -0400

    Modified Basics Solution; added Iterating_While_Do_For Project

---
+++ dev/languages/client-server/cpp/foundation/basics/Iterating_While_Do_For/Iterating_While_Do_For.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//while (true)
+	//{
+	//	// Note, the program will never exit this.
+
+	//	// Therefore, anything below is never executed.
+	//}
+
+	int ctr = 0;
+	while (ctr <= 1000)
+	{
+		cout << ctr << " ";
+		ctr++;
+	}
+
+	string password = "secret";
+	string guess = "";
+	do
+	{
+		cout << "\nEnter password: ";
+		cin >> guess;
+	} while (password != guess);
+
+	cout << "Access granted..." << endl;
+
+	for (int i = 0; i <= 10; i++)
+	{
+		cout << i << endl;
+	}
+
+	//for (;;)
+	//{
+	//	// As with the above while (true) statement {} => to infinity and beyond...
+	//}
+}
