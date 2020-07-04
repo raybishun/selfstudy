372791acc97451e6818af28266b10f355542e1e7
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-07-04 11:56:53 -0400

    Added Pointers Project

---
+++ dev/languages/client-server/cpp/foundation/basics/Pointers/Pointers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int myInt = 618;
+
+    // '&myInt' returns the 32 or 64-bit (hexadecimal) memory address
+    //  that was 'dynamically' allocated to the myInt variable
+    cout << &myInt << endl;
+
+    // A pointer is used to store the 'memory address' of myInt
+    // int* denotes a pointer
+    int* myIntPtr;
+    myIntPtr = &myInt;
+    cout << myIntPtr << endl;
+
+    system("pause");
+}
