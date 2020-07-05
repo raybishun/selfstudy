f03202e3361d842c4fbfce07b9a80143b3a24272
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-07-05 00:59:35 -0400

    Added Java HelloWorld

---
+++ dev/languages/client-server/java/foundation/HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+    }
+}
+
+// To compile to Java bytecode and run from a CMD prompt:
+//  1. c:\> javac .\HelloWorld.java (a HelloWorld.class will be created)
+//  2. c:\> java .\HelloWorld (from a CMD prompt, NOT PowerShell)
+
+// To Configure the Java runtime:
+// 1. CTRL + Shift + P
+// 2. Enter:  Java: Configure Java Runtime
+
+// Getting Started
+// 1. CTRL + Shift + P
+// 2. Java: Getting Started
