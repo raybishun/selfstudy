1cd46019e0dc5b126e5d524e6bd74f09244eeb63
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-07-06 21:23:52 -0400

    Added 05_input_output_operations: typecasting

---
+++ dev/languages/client-server/py/foundation/python_entry_level2/05_input_output_operations.py
@@ -0,0 +1,27 @@
+# Typecasting
+# =============================================================================
+print(float(1))  # 1.0
+
+print(int(1.618)) # 1
+
+print(str(1.618)) # 'a string value of '1.618'
+
+print(str(True)) # 'True'
+
+print(int('5')) # 5 as an int
+
+# Doesn't truncate the decimal portion, actually returns: 
+# Exception has occurred: ValueError: invalid literal for int() with base 10: '1.618'
+# print(int('1.618')) 
+
+# Interesting 'bool' behavior:
+#   practically everything returns true,
+#   except for anything that returns 0 or that is 0 length
+print(bool(1)) # true
+print(bool(1.618)) # true
+print(bool('abc')) # true
+print(bool(0)) # false
+print(bool('')) # false
+
+print(1 and 0) # 0
+print(1 or 0) # 1
