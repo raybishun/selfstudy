35bafc6887d35937a1564787bbbea6da4fb735db
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-07-18 20:50:06 -0400

    Added cosmos_db_storage

---
+++ dev/cloud/azure_dev/cosmos_db_storage/README.md
@@ -0,0 +1,22 @@
+# Azure Cosmos DB
+
+### What Cosmos DB?
+- A globally distributed, multi-model database server
+
+### Features
+1. Significantly more powerful and feature rich than Azure Storage Tables
+2. Low latency
+3. Highly available
+4. Elastic scalability
+5. Multi-modal
+6. Provides single-digit-millisecond data access
+7. Supports several API surfaces including: SQL, MongoDB, Cassandra, Table and Gremlin
+
+### Create and run a new console app
+- mkdir helloworld
+- cd .\helloworld\
+- dotnet new console
+- dotnet build
+- dotnet run
+
+### References
---
+++ dev/cloud/azure_dev/cosmos_db_storage/config.ps1
@@ -0,0 +1,47 @@
+# login in Azure
+# -----------------------------------------------------------------------------
+az login
+
+# Init Variables
+$resourceGroupName = "cosmosdb"
+$location = "eastus"
+$accountName= "laazcosmosdb"
+$databaseName = "myDatabase"
+
+az group create `
+ -n $resourceGroupName `
+ -l $location
+
+# Create a SQL API Cosmos DB account with session consistency and multi-master enabled
+az cosmosdb create `
+ -g $resourceGroupName `
+ --name $accountName `
+ --kind GlobalDocumentDB `
+ --locations "West US=0" "North East US=1" `
+ --default-consistency-level Strong `
+ --enable-multiple-write-locations true `
+ --enable-automatic-failover true
+
+# Create a database
+az cosmosdb database create `
+ -g $resourceGroupName `
+ --name $accountName `
+ --db-name $databaseName
+
+# List account keys
+az cosmosdb list-keys `
+ --name $accountName `
+ -g $resourceGroupName
+
+# List account connection strings
+az cosmosdb list-connection-strings `
+ --name $accountName `
+ -g $resourceGroupName
+
+az cosmosdb show `
+ --name $accountName `
+ -g $resourceGroupName `
+ --query "documentEndpoint"
+
+# Clean up
+az group delete -y -g $resourceGroupName
