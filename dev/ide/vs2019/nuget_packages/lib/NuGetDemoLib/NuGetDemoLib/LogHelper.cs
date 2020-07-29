7c4bc8673f72e3c7f88367273c102758b80b10ee
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-07-29 11:58:29 -0400

    Created NuGetDemo Library

---
+++ dev/ide/vs2019/nuget_packages/lib/NuGetDemoLib/NuGetDemoLib/LogHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NuGetDemoLib
+{
+    public class LogHelper
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
