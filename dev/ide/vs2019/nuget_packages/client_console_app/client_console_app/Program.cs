3f3f30b832c56351968a093da01c0732a5547b17
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-07-31 23:13:18 -0400

    Added client_console_app

---
+++ dev/ide/vs2019/nuget_packages/client_console_app/client_console_app/Program.cs
@@ -0,0 +1,12 @@
+﻿using NuGetDemoLib;
+
+namespace client_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogHelper.Log("Hello from NuGetDemoLib :)");
+        }
+    }
+}
