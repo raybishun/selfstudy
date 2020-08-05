b9a0ff465277e472737814caab64190bd74ed2f6
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-08-05 07:51:24 -0400

    Added PrgInCS2020

---
+++ dev/languages/client-server/cs/foundation/PrgInCS2020/ManagePrgFlow/ManagePrgFlow/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace ManagePrgFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Task1()
+        {
+            Console.WriteLine("Task 1 starting...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task 1 ending.");
+        }
+    }
+}
