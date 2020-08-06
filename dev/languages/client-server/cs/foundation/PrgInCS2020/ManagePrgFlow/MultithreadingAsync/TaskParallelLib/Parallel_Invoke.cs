6d264e165a4dceaac7efc3927c1887584790c51d
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-08-05 21:39:01 -0400

    Added MultithreadingAsync.TaskParallelLib

---
+++ dev/languages/client-server/cs/foundation/PrgInCS2020/ManagePrgFlow/MultithreadingAsync/TaskParallelLib/Parallel_Invoke.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace TaskParallelLib
+{
+    class Parallel_Invoke
+    {
+        // Parallel.Invoke() accepts Action delegates
+        // and creates a task for each of them.
+
+        // Action delegates accepts no parameters, 
+        // and do not return a result.
+
+        public static void Task1()
+        {
+            Console.WriteLine("Task-1 starting...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task-1 end.");
+        }
+
+        public static void Task2()
+        {
+            Console.WriteLine("Task-2 starting...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task-2 end.");
+        }
+    }
+}
---
+++ dev/languages/client-server/cs/foundation/PrgInCS2020/ManagePrgFlow/MultithreadingAsync/TaskParallelLib/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskParallelLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(() => Parallel_Invoke.Task1(), () => Parallel_Invoke.Task2());
+
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+        }
+    }
+}
