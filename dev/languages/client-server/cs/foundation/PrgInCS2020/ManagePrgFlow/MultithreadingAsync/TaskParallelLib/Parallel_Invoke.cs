eee6dd0e63f686205faa491cd3f89e005141337e
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-08-06 20:34:18 -0400

    Reviewed TPL's Task Class, Parallel.Invoke()

--- dev/languages/client-server/cs/foundation/PrgInCS2020/ManagePrgFlow/MultithreadingAsync/TaskParallelLib/Parallel_Invoke.cs
+++ dev/languages/client-server/cs/foundation/PrgInCS2020/ManagePrgFlow/MultithreadingAsync/TaskParallelLib/Parallel_Invoke.cs
@@ -3,14 +3,8 @@ using System.Threading;
 
 namespace TaskParallelLib
 {
-    class Parallel_Invoke
+    class BunchOfTasks
     {
-        // Parallel.Invoke() accepts Action delegates
-        // and creates a task for each of them.
-
-        // Action delegates accepts no parameters, 
-        // and do not return a result.
-
         public static void Task1()
         {
             Console.WriteLine("Task-1 starting...");
@@ -23,6 +17,59 @@ namespace TaskParallelLib
             Console.WriteLine("Task-2 starting...");
             Thread.Sleep(1000);
             Console.WriteLine("Task-2 end.");
+        }
+        public static void Task3()
+        {
+            Console.WriteLine("Task-3 starting...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task-3 end.");
+        }
+
+        public static void Task4()
+        {
+            Console.WriteLine("Task-4 starting...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task-4 end.");
+        }
+        public static void Task5()
+        {
+            Console.WriteLine("Task-5 starting...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task-5 end.");
+        }
+
+        public static void Task6()
+        {
+            Console.WriteLine("Task-6 starting...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task-6 end.");
+        }
+        public static void Task7()
+        {
+            Console.WriteLine("Task-7 starting...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task-7end.");
+        }
+
+        public static void Task8()
+        {
+            Console.WriteLine("Task-8 starting...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task-8end.");
+        }
+
+        public static void Task9()
+        {
+            Console.WriteLine("Task-9 starting...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task-9 end.");
+        }
+
+        public static void Task10()
+        {
+            Console.WriteLine("Task-10 starting...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task-10 end.");
         }
     }
 }
--- dev/languages/client-server/cs/foundation/PrgInCS2020/ManagePrgFlow/MultithreadingAsync/TaskParallelLib/Program.cs
+++ dev/languages/client-server/cs/foundation/PrgInCS2020/ManagePrgFlow/MultithreadingAsync/TaskParallelLib/Program.cs
@@ -7,11 +7,41 @@ namespace TaskParallelLib
     {
         static void Main(string[] args)
         {
-            Parallel.Invoke(() => Parallel_Invoke.Task1(),
-                            () => Parallel_Invoke.Task2());
+            // The Task.Parallel class in the TPL (Task Parallel Library),
+            // provides 3 methods that can be used to execute tasks in parallel
 
-            Console.WriteLine("Done.");
+            Tasks_Parallel_Invoke();
+
+
             Console.ReadKey();
         }
+
+        static void Tasks_Parallel_Invoke()
+        {
+            // Parallel.Invoke() accepts Action delegates
+            // and creates a task for each of them.
+
+            // Action delegates accepts no parameters, 
+            // and do not return a result.
+
+            // Also, the Action delegates can be replaced
+            // with lambda expressions, as shown below
+
+            // Note, can cannot control/run a task on a  
+            // particular core, the OS determines this
+            Parallel.Invoke(() => BunchOfTasks.Task1(),
+                            () => BunchOfTasks.Task2(),
+                            () => BunchOfTasks.Task3(),
+                            () => BunchOfTasks.Task4(),
+                            () => BunchOfTasks.Task5(),
+                            () => BunchOfTasks.Task6(),
+                            () => BunchOfTasks.Task7(),
+                            () => BunchOfTasks.Task8(),
+                            () => BunchOfTasks.Task9(),
+                            () => BunchOfTasks.Task10());
+
+            Console.WriteLine("Done.");
+        }
+
     }
 }
