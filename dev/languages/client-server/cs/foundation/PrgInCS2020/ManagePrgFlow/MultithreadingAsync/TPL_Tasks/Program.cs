518bf407f35cc070516fcb4560775fa2493f4cf8
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-08-10 19:38:19 -0400

    Added a TPL_Tasks project

---
+++ dev/languages/client-server/cs/foundation/PrgInCS2020/ManagePrgFlow/MultithreadingAsync/TPL_Tasks/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPL_Tasks
+{
+    class Program
+    {
+        static void SomeWork()
+        {
+            Console.WriteLine("Stating work...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Work completed.");
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+        
+        static void Main(string[] args)
+        {
+            DoWork();
+        }
+
+        static void DoWork()
+        {
+            Task t = new Task(() => SomeWork());
+            t.Start();
+            t.Wait();
+        }
+    }
+}
