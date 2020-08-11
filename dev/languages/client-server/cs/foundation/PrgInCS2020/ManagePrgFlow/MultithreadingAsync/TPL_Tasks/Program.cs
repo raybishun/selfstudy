5be4826da8b8f748a32228713008c2b55ef8be1e
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-08-10 20:44:33 -0400

    Modified TPL_Tasks project

--- dev/languages/client-server/cs/foundation/PrgInCS2020/ManagePrgFlow/MultithreadingAsync/TPL_Tasks/Program.cs
+++ dev/languages/client-server/cs/foundation/PrgInCS2020/ManagePrgFlow/MultithreadingAsync/TPL_Tasks/Program.cs
@@ -6,26 +6,35 @@ namespace TPL_Tasks
 {
     class Program
     {
-        static void SomeWork()
+        static void Main(string[] args)
         {
-            Console.WriteLine("Stating work...");
-            Thread.Sleep(2000);
-            Console.WriteLine("Work completed.");
+            // Run a task within a method (Main() in this case)
+            //Task t = Task.Run(() => Work.SomeWork());
+            //t.Wait();
+
+            // DoSomeWork();
+            Return_A_Value_From_A_Task();
+
 
             Console.WriteLine("Done");
             Console.ReadKey();
         }
-        
-        static void Main(string[] args)
-        {
-            DoWork();
-        }
 
-        static void DoWork()
+        static void DoSomeWork()
         {
-            Task t = new Task(() => SomeWork());
+            Task t = new Task(() => Work.SomeWork());
             t.Start();
             t.Wait();
+        }
+
+        static void Return_A_Value_From_A_Task()
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return Work.CalcResult();
+            });
+
+            Console.WriteLine(t.Result);
         }
     }
 }
---
+++ dev/languages/client-server/cs/foundation/PrgInCS2020/ManagePrgFlow/MultithreadingAsync/TPL_Tasks/Work.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TPL_Tasks
+{
+    class Work
+    {
+        public static void SomeWork()
+        {
+            Console.WriteLine("Stating work...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Work completed.");
+        }
+
+        public static int CalcResult()
+        {
+            Console.WriteLine("Stating work...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Work completed.");
+            return 99;
+        }
+    }
+}
