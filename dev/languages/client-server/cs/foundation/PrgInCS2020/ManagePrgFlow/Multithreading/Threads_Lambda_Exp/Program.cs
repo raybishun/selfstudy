14693052f3d25942960f1285604b8acc3b9835e6
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-08-16 20:22:47 -0400

    Created Threads_Lambda_Exp project

---
+++ dev/languages/client-server/cs/foundation/PrgInCS2020/ManagePrgFlow/Multithreading/Threads_Lambda_Exp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads_Lambda_Exp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Series: Managing Program Flow
+            // Section: Implement multithreading and asynchronous processing
+            // Sub-Section: Threads and ThreadPool
+            // ===========================================================
+
+            // Topic: Starting a thread using a lambda expression
+            // -----------------------------------------------------------
+
+            Thread t = new Thread(() =>
+            {
+                Console.WriteLine("Hello World!");
+                Thread.Sleep(1000);
+            });
+
+            // The main thread starts a new background thread
+            t.Start();
+
+            // The main thread will also continue and execute the below 
+            Console.WriteLine("Done");
+        }
+    }
+}
