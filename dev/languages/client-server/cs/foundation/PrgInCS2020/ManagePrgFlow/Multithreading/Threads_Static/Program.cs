5435db1af8f15ba33ed154453bc85595ce58b4e6
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-08-20 20:56:39 -0400

    Created Thread_Local project

---
+++ dev/languages/client-server/cs/foundation/PrgInCS2020/ManagePrgFlow/Multithreading/Threads_Static/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads_Local
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Series: Managing Program Flow
+            // Section: Implement multithreading and asynchronous processing
+            // Sub-Section: Threads and ThreadPool
+            // ===========================================================
+
+            // Topic: The ThreadLocal Class
+            // The ThreadLocal class provides thread-local storage of data
+            // -----------------------------------------------------------
+
+        }
+
+        static ThreadLocal<Random> RandomGenerator = new ThreadLocal<Random>(() =>
+        {
+            return new Random(2);
+        });
+    }
+}
