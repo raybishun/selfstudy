c142b1b01f841aa7e1b701ace432a83c206ec4c9
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-08-31 22:03:10 -0400

    Added initial CharacterController.cs, Character.cs, and RpgClass.cs

---
+++ dev/languages/client-server/cs/dotnetcore/dotnetcore_webapi_ef_jumpstart/dotnet-rpg/Controllers/CharacterController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using dotnet_rpg.Models;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static Character knight = new Character();
+
+        public IActionResult Get()
+        {
+            return Ok(knight);
+
+            // dotnet run
+            // Postman GET: http://localhost:5000/character
+        }
+    }
+}
