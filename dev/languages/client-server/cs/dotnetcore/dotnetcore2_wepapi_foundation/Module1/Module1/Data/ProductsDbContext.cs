5b91686a717827abc394a2fc4688b19f66f4a301
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-09-07 19:41:15 -0400

    Added a new ProductsController with ProductDbContext

--- dev/languages/client-server/cs/dotnetcore/dotnetcore2_wepapi_foundation/Module1/Module1/Data/ProductsDbContext.cs
+++ dev/languages/client-server/cs/dotnetcore/dotnetcore2_wepapi_foundation/Module1/Module1/Data/ProductsDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Module1.Models;
 
 namespace Module1.Data
 {
     public class ProductsDbContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
     }
 }
