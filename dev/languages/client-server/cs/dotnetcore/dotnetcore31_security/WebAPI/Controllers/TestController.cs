0fd69632c908ca2d5664bb890eee388a2aa75bbc
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-09-17 21:57:50 -0400

    Added WebAPI Project

---
+++ dev/languages/client-server/cs/dotnetcore/dotnetcore31_security/WebAPI/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            // Using a hosted REST-API to respond to requests at: https://reqres.in/
+            string baseUrl = "https://reqres.in/";
+            string resource = "/api/users/2";
+
+            IRestClient client = new RestClient(baseUrl);
+            IRestRequest request = new RestRequest(resource, DataFormat.Json);
+            IRestResponse response = client.Get(request);
+
+            return new string[] { response.Content };
+        }
+    }
+}
