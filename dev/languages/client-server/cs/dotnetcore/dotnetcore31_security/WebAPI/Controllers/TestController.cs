137ae6e7c03238baabce71c0d28f7fd202688643
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-09-18 22:12:45 -0400

    Deserialize<User> based on response

--- dev/languages/client-server/cs/dotnetcore/dotnetcore31_security/WebAPI/Controllers/TestController.cs
+++ dev/languages/client-server/cs/dotnetcore/dotnetcore31_security/WebAPI/Controllers/TestController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RestSharp;
+using RestSharp.Deserializers;
+using RestSharp.Serialization.Json;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -12,20 +16,27 @@ namespace WebAPI.Controllers
     [ApiController]
     public class TestController : ControllerBase
     {
-        [HttpGet]
+        // Using a hosted REST-API to respond to requests at: https://reqres.in/
+        static string baseUrl = "https://reqres.in/";
+        static string resource = "/api/users/2";
+
+        static IRestClient client = new RestClient(baseUrl);
+        static IRestRequest request = new RestRequest(resource, DataFormat.Json);
+        static IRestResponse response = client.Get(request);
+
         public IEnumerable<string> Get()
         {
-            // Using a hosted REST-API to respond to requests at: https://reqres.in/
-            string baseUrl = "https://reqres.in/";
-            string resource = "/api/users/2";
-
-            IRestClient client = new RestClient(baseUrl);
-            IRestRequest request = new RestRequest(resource, DataFormat.Json);
-            IRestResponse response = client.Get(request);
-
-            // TODO: Create/serialize user object
-
             return new string[] { response.Content };
+        }
+
+        [HttpGet("GetUser")]
+        public User Get_User()
+        {
+            var d = new JsonDeserializer();
+
+            var ouput = d.Deserialize<User>(response);
+
+            return ouput;
         }
     }
 }
