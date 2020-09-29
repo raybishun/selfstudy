0c581498f033604fea5daec16147c829ed308af3
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-09-28 20:35:12 -0400

    Added SignUp Post method and corresponding View

---
+++ dev/languages/client-server/cs/dotnetcore/identity/IdentityNetCore/IdentityNetCore/Controllers/IdentityController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityNetCore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityNetCore.Controllers
+{
+    public class IdentityController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public IdentityController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> SignUp()
+        {
+            var model = new SignUpViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if ( (await _userManager.FindByEmailAsync(model.Email)) != null )
+                {
+                    var user = new IdentityUser {Email = model.Email, UserName = model.Email};
+                    
+                    var result = await _userManager.CreateAsync(user, model.Password);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("SignIn");
+                    }
+
+                    ModelState.AddModelError(
+                        "SignUp", string.Join("", result.Errors.Select( x=> x.Description)));
+
+                    return View(model);
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> SignIn()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> AccessDenied()
+        {
+            return View();
+        }
+    }
+}
