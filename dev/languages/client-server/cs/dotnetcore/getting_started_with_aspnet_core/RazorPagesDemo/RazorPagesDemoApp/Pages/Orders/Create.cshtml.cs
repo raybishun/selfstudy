163fbe998dd7885f49c4e2403e6bfaa1b08344f7
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-10-23 19:46:13 -0400

    Added Create Razor Page with CreateModel

---
+++ dev/languages/client-server/cs/dotnetcore/getting_started_with_aspnet_core/RazorPagesDemo/RazorPagesDemoApp/Pages/Orders/Create.cshtml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLibrary.Data;
+using DataLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RazorPagesDemoApp.Pages.Orders
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IFoodData _foodData;
+        private readonly IOrderData _orderData;
+
+        public List<SelectListItem> FoodItems { get; set; }
+
+        [BindProperty]
+        public OrderModel Order { get; set; }
+
+        public CreateModel(IFoodData foodData, IOrderData orderData)
+        {
+            _foodData = foodData;
+            _orderData = orderData;
+        }
+
+        public async Task OnGet()
+        {
+            var food = await _foodData.GetFood();
+
+            FoodItems = new List<SelectListItem>();
+
+            food.ForEach(x => 
+            {
+                FoodItems.Add(new SelectListItem { Value = x.Id.ToString(), Text = x.Title });
+            });
+        }
+    }
+}
