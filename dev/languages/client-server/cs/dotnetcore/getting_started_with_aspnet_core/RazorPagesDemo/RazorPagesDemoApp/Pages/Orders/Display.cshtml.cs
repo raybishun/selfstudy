bed6457ef21a312bc6ac215569583b26c14f19f1
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-10-26 22:27:06 -0400

    Started on the Display Razor Page

---
+++ dev/languages/client-server/cs/dotnetcore/getting_started_with_aspnet_core/RazorPagesDemo/RazorPagesDemoApp/Pages/Orders/Display.cshtml.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using DataLibrary.Data;
+using DataLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesDemoApp.Pages.Orders
+{
+    public class DisplayModel : PageModel
+    {
+        private readonly IOrderData _orderData;
+        private readonly IFoodData _foodData;
+
+        [BindProperty(SupportsGet = true)]
+        public int Id { get; set; }
+
+        public OrderModel Order { get; set; }
+        public string ItemPurchased { get; set; }
+
+        public DisplayModel(IOrderData orderData, IFoodData foodData)
+        {
+            _orderData = orderData;
+            _foodData = foodData;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
