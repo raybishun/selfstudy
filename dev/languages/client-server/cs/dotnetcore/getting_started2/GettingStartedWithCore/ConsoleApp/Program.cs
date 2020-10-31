cf50fbc71f370859a07a77ea61e163b85f88156a
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-10-30 22:30:26 -0400

    Create new Solution: GettingStartedWithCore, looked at parsing the appsetting.json file

---
+++ dev/languages/client-server/cs/dotnetcore/getting_started2/GettingStartedWithCore/ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static IConfiguration Configuration;
+
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+            Console.WriteLine("Hello World!");
+
+            string connectionString = Configuration.GetConnectionString("Default");
+            Console.WriteLine($"ConnectionStrings value: {connectionString}");
+
+            string otherInfo = Configuration.GetSection("OtherInfo").Value;
+            Console.WriteLine($"OtherInfo value: {otherInfo}");
+
+            string demo1 = Configuration.GetSection("Demos").GetSection("Demo1").Value;
+            Console.WriteLine($"Demo1 value: {demo1}");
+        }
+    }
+}
