9af76ff8a2968e7a8d20bedbbce30d23a7de1ba8
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-10-31 19:30:09 -0400

    Added WebApp to show HTML and Tag Helpers (DemoHtmlAndTagHelpersApp)

---
+++ dev/languages/client-server/cs/dotnetcore/getting_started2/GettingStartedWithCore/WebAPI/Controllers/MoreInfoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoreInfoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "one", "two", "three"};
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return $"Id: {id}";
+        }
+    }
+}
