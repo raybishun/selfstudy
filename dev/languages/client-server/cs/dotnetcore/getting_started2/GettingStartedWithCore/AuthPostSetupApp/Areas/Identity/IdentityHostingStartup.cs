60cb44835b68dd17b2df5aeaefe9b385377d5ea1
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-11-01 14:43:40 -0500

    Added AuthPostSetupApp project to show how to add auth after a web app has been created

---
+++ dev/languages/client-server/cs/dotnetcore/getting_started2/GettingStartedWithCore/AuthPostSetupApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AuthPostSetupApp.Areas.Identity.Data;
+using AuthPostSetupApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthPostSetupApp.Areas.Identity.IdentityHostingStartup))]
+namespace AuthPostSetupApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthPostSetupAppContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("AuthPostSetupAppContextConnection")));
+
+                services.AddDefaultIdentity<AuthPostSetupAppUser>()
+                    .AddEntityFrameworkStores<AuthPostSetupAppContext>();
+            });
+        }
+    }
+}
