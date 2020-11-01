6da87be0d8830f48f1031715ce6f1b334b6f8406
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-10-31 20:06:31 -0400

    Added OnGEtAnync() to DataDemo.cshtml.cs to show how to get and dispaly data on a Razor Page

---
+++ dev/languages/client-server/cs/dotnetcore/getting_started2/GettingStartedWithCore/DemoHtmlAndTagHelpersApp/Pages/DataDemo.cshtml.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoHtmlAndTagHelpersApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoHtmlAndTagHelpersApp.Pages
+{
+    public class DataDemoModel : PageModel
+    {
+        public List<PersonModel> People { get; set; } = new List<PersonModel>();
+        
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; } = "";
+
+        public async Task OnGetAsync()
+        {
+            List<PersonModel> output = new List<PersonModel>();
+
+            output.Add(new PersonModel { FirstName = "Peter", LastName = "Parker" });
+            output.Add(new PersonModel { FirstName = "Bruce", LastName = "Wayne" });
+            output.Add(new PersonModel { FirstName = "Ray", LastName = "Bishun" });
+
+            if (string.IsNullOrWhiteSpace(SearchTerm) == false)
+            {
+                People = output.Where(x => x.LastName.StartsWith(SearchTerm)).ToList();
+            }
+            else
+            {
+                // Return the full list
+                // People = output;
+            }
+        }
+    }
+}
