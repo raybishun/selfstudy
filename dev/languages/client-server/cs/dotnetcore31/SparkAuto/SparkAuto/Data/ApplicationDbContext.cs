f83fabb117cad400a87843ff5f2d59abfa4d2efb
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-11-12 19:29:02 -0500

    Created the ServiceType model

--- dev/languages/client-server/cs/dotnetcore31/SparkAuto/SparkAuto/Data/ApplicationDbContext.cs
+++ dev/languages/client-server/cs/dotnetcore31/SparkAuto/SparkAuto/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SparkAuto.Models;
 
 namespace SparkAuto.Data
 {
@@ -11,6 +12,12 @@ namespace SparkAuto.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            
         }
+
+        public DbSet<ServiceType> ServiceType { get; set; }
+        // In the PM Console, don't forget to run:
+        //  1/2: Add-Migration AddServiceTypeToTheDB
+        //  2/2: Update-Database
     }
 }
