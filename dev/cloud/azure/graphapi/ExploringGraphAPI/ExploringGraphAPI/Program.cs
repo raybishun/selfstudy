8366faaef317ede771ac52393274dec462410541
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-11-25 23:48:15 -0500

    Added GetUnFilteredMessages() and GetFilteredMessage()

--- dev/cloud/azure/graphapi/ExploringGraphAPI/ExploringGraphAPI/Program.cs
+++ dev/cloud/azure/graphapi/ExploringGraphAPI/ExploringGraphAPI/Program.cs
@@ -1,18 +1,12 @@
-﻿using Microsoft.Graph;
-using System;
+﻿using System;
 using System.Net.Http.Headers;
+using Microsoft.Graph;
 
 namespace ExploringGraphAPI
 {
     class Program
     {
-        // Use Graph Explorer to get your token: https://developer.microsoft.com/en-us/graph/graph-explorer
-        static readonly string _accessToken = "eyJ0eXAiOiJKV1QiLCJub25jZSI6ImVNVFEtRWJvZ2FISS1nV0NrZHJVZ3pyX09udUlTSFJhOVF3TmdSblUxeTgiLCJhbGciOiJSUzI1NiIsIng1dCI6ImtnMkxZczJUMENUaklmajRydDZKSXluZW4zOCIsImtpZCI6ImtnMkxZczJUMENUaklmajRydDZKSXluZW4zOCJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8xODBkMGFiMy0zMjBmLTQ5NzktODQxYS1hNzU5MDEwNWUzYTIvIiwiaWF0IjoxNjA2MzYwMzczLCJuYmYiOjE2MDYzNjAzNzMsImV4cCI6MTYwNjM2NDI3MywiYWNjdCI6MCwiYWNyIjoiMSIsImFjcnMiOlsidXJuOnVzZXI6cmVnaXN0ZXJzZWN1cml0eWluZm8iLCJ1cm46bWljcm9zb2Z0OnJlcTEiLCJ1cm46bWljcm9zb2Z0OnJlcTIiLCJ1cm46bWljcm9zb2Z0OnJlcTMiLCJjMSIsImMyIiwiYzMiLCJjNCIsImM1IiwiYzYiLCJjNyIsImM4IiwiYzkiLCJjMTAiLCJjMTEiLCJjMTIiLCJjMTMiLCJjMTQiLCJjMTUiLCJjMTYiLCJjMTciLCJjMTgiLCJjMTkiLCJjMjAiLCJjMjEiLCJjMjIiLCJjMjMiLCJjMjQiLCJjMjUiXSwiYWlvIjoiQVNRQTIvOFJBQUFBNm8vTEgrbVRFVkEzckVyS3hpZ0RQTEtzaG1HaTVNRWx3RkxNSVNyQ243Zz0iLCJhbXIiOlsicHdkIl0sImFwcF9kaXNwbGF5bmFtZSI6IkdyYXBoIGV4cGxvcmVyIChvZmZpY2lhbCBzaXRlKSIsImFwcGlkIjoiZGU4YmM4YjUtZDlmOS00OGIxLWE4YWQtYjc0OGRhNzI1MDY0IiwiYXBwaWRhY3IiOiIwIiwiZmFtaWx5X25hbWUiOiJCaXNodW4iLCJnaXZlbl9uYW1lIjoiUmF5IiwiaWR0eXAiOiJ1c2VyIiwiaXBhZGRyIjoiMjQuMTAyLjExMy4xOTciLCJuYW1lIjoiUmF5IEJpc2h1biIsIm9pZCI6IjU4MDBjZjNkLWQzMDEtNDQ2Yy1iZWU2LWEwNjM4ZGZjYzQyNyIsInBsYXRmIjoiMyIsInB1aWQiOiIxMDAzMjAwMDQ5RTg0RTFEIiwicmgiOiIwLkFUY0Fzd29OR0E4eWVVbUVHcWRaQVFYam9yWElpOTc1MmJGSXFLMjNTTnB5VUdRM0FBMC4iLCJzY3AiOiJDYWxlbmRhcnMuUmVhZFdyaXRlIENvbnRhY3RzLlJlYWRXcml0ZSBGaWxlcy5SZWFkV3JpdGUuQWxsIE1haWwuUmVhZFdyaXRlIE1haWwuU2VuZCBOb3Rlcy5SZWFkV3JpdGUuQWxsIG9wZW5pZCBQZW9wbGUuUmVhZCBwcm9maWxlIFNlY3VyaXR5RXZlbnRzLlJlYWQuQWxsIFNlY3VyaXR5RXZlbnRzLlJlYWRXcml0ZS5BbGwgU2l0ZXMuUmVhZFdyaXRlLkFsbCBUYXNrcy5SZWFkV3JpdGUgVXNlci5SZWFkIFVzZXIuUmVhZEJhc2ljLkFsbCBVc2VyLlJlYWRXcml0ZSBlbWFpbCIsInNpZ25pbl9zdGF0ZSI6WyJpbmtub3dubnR3ayJdLCJzdWIiOiJWdHJZVlpmZHk0ZU05XzV1dHVBREx0cjZEUlB5dy1wQU43RXp1c045YXZBIiwidGVuYW50X3JlZ2lvbl9zY29wZSI6Ik5BIiwidGlkIjoiMTgwZDBhYjMtMzIwZi00OTc5LTg0MWEtYTc1OTAxMDVlM2EyIiwidW5pcXVlX25hbWUiOiJyYXkuYmlzaHVuQGJpdHNieXRlcy5jb20iLCJ1cG4iOiJyYXkuYmlzaHVuQGJpdHNieXRlcy5jb20iLCJ1dGkiOiJCc1k0dVhmYmswRzA3WGNIRjlfVkFBIiwidmVyIjoiMS4wIiwid2lkcyI6WyIxOTRhZTRjYi1iMTI2LTQwYjItYmQ1Yi02MDkxYjM4MDk3N2QiLCJlODYxMWFiOC1jMTg5LTQ2ZTgtOTRlMS02MDIxM2FiMWY4MTQiLCI2MmU5MDM5NC02OWY1LTQyMzctOTE5MC0wMTIxNzcxNDVlMTAiLCJiNzlmYmY0ZC0zZWY5LTQ2ODktODE0My03NmIxOTRlODU1MDkiXSwieG1zX3N0Ijp7InN1YiI6Imdhb3RXUi1Oc0I3Nmktek90N3JPeFBxVkZPMkhiN0VJaWY2Z0hvSmtoaG8ifSwieG1zX3RjZHQiOjE1NTg4Mjc3OTF9.mCXKyaJT00FroXlPQVOOrE04nDf4v622pgynwgnK85kuCVqKidTiBotfBRcvpi6Xle5W4FXH1703dvHmkcwimd6ee9lJYZS5h7rTIzC6Si22PgZjieIlVDPedtiylWzmxNUpFiNvKxLhBDXbIdsw-pI-SXP0TUo6pb4hHDYB8tHZOFxYvQSSLs-TE5E_QEv2Vm7zuqv3PhDJNA5PPwslKIqK2fxFLivKuZxrrRBVkA7WMnZvj2oPZwtAgnmcgM54n8l8FaV6HsoFQ9XvLoDwdLdHLlBUkCNL2fgpcwFSh_n7Ojjy4CWXH0B8Ob43IxxW10jBZbGSCECd01I8atfsIw";
-
-        static readonly GraphServiceClient _client = new GraphServiceClient(new DelegateAuthenticationProvider(async request => {
-            request.Headers.Authorization = new AuthenticationHeaderValue("bearer", _accessToken);
-        }));
-
+        
         static void Main(string[] args)
         {
             GetUnFilteredMessages();
@@ -20,9 +14,24 @@ namespace ExploringGraphAPI
             SendMessage();
         }
 
+        static string GetToken()
+        {
+            // Use Graph Explorer to get your temporary token: https://developer.microsoft.com/en-us/graph/graph-explorer
+            return "eyJ0eXAiOiJKV1QiLCJub25jZSI6Ii1JNUtGNTlxMDlrUVhaZmhKcTJZNjdTeDluaGJXdjhSOG9fMnc2UWpsMG8iLCJhbGciOiJSUzI1NiIsIng1dCI6ImtnMkxZczJUMENUaklmajRydDZKSXluZW4zOCIsImtpZCI6ImtnMkxZczJUMENUaklmajRydDZKSXluZW4zOCJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8xODBkMGFiMy0zMjBmLTQ5NzktODQxYS1hNzU5MDEwNWUzYTIvIiwiaWF0IjoxNjA2MzYzODkwLCJuYmYiOjE2MDYzNjM4OTAsImV4cCI6MTYwNjM2Nzc5MCwiYWNjdCI6MCwiYWNyIjoiMSIsImFjcnMiOlsidXJuOnVzZXI6cmVnaXN0ZXJzZWN1cml0eWluZm8iLCJ1cm46bWljcm9zb2Z0OnJlcTEiLCJ1cm46bWljcm9zb2Z0OnJlcTIiLCJ1cm46bWljcm9zb2Z0OnJlcTMiLCJjMSIsImMyIiwiYzMiLCJjNCIsImM1IiwiYzYiLCJjNyIsImM4IiwiYzkiLCJjMTAiLCJjMTEiLCJjMTIiLCJjMTMiLCJjMTQiLCJjMTUiLCJjMTYiLCJjMTciLCJjMTgiLCJjMTkiLCJjMjAiLCJjMjEiLCJjMjIiLCJjMjMiLCJjMjQiLCJjMjUiXSwiYWlvIjoiQVNRQTIvOFJBQUFBd0hPZDQxNmVIM05ETnh2cGNML3p4TlFzMk1EN2hWQWxBRkRKWjFLV2xKRT0iLCJhbXIiOlsicHdkIl0sImFwcF9kaXNwbGF5bmFtZSI6IkdyYXBoIGV4cGxvcmVyIChvZmZpY2lhbCBzaXRlKSIsImFwcGlkIjoiZGU4YmM4YjUtZDlmOS00OGIxLWE4YWQtYjc0OGRhNzI1MDY0IiwiYXBwaWRhY3IiOiIwIiwiZmFtaWx5X25hbWUiOiJCaXNodW4iLCJnaXZlbl9uYW1lIjoiUmF5IiwiaWR0eXAiOiJ1c2VyIiwiaXBhZGRyIjoiMjQuMTAyLjExMy4xOTciLCJuYW1lIjoiUmF5IEJpc2h1biIsIm9pZCI6IjU4MDBjZjNkLWQzMDEtNDQ2Yy1iZWU2LWEwNjM4ZGZjYzQyNyIsInBsYXRmIjoiMyIsInB1aWQiOiIxMDAzMjAwMDQ5RTg0RTFEIiwicmgiOiIwLkFUY0Fzd29OR0E4eWVVbUVHcWRaQVFYam9yWElpOTc1MmJGSXFLMjNTTnB5VUdRM0FBMC4iLCJzY3AiOiJDYWxlbmRhcnMuUmVhZFdyaXRlIENvbnRhY3RzLlJlYWRXcml0ZSBGaWxlcy5SZWFkV3JpdGUuQWxsIE1haWwuUmVhZFdyaXRlIE1haWwuU2VuZCBOb3Rlcy5SZWFkV3JpdGUuQWxsIG9wZW5pZCBQZW9wbGUuUmVhZCBwcm9maWxlIFNlY3VyaXR5RXZlbnRzLlJlYWQuQWxsIFNlY3VyaXR5RXZlbnRzLlJlYWRXcml0ZS5BbGwgU2l0ZXMuUmVhZFdyaXRlLkFsbCBUYXNrcy5SZWFkV3JpdGUgVXNlci5SZWFkIFVzZXIuUmVhZEJhc2ljLkFsbCBVc2VyLlJlYWRXcml0ZSBlbWFpbCIsInNpZ25pbl9zdGF0ZSI6WyJpbmtub3dubnR3ayJdLCJzdWIiOiJWdHJZVlpmZHk0ZU05XzV1dHVBREx0cjZEUlB5dy1wQU43RXp1c045YXZBIiwidGVuYW50X3JlZ2lvbl9zY29wZSI6Ik5BIiwidGlkIjoiMTgwZDBhYjMtMzIwZi00OTc5LTg0MWEtYTc1OTAxMDVlM2EyIiwidW5pcXVlX25hbWUiOiJyYXkuYmlzaHVuQGJpdHNieXRlcy5jb20iLCJ1cG4iOiJyYXkuYmlzaHVuQGJpdHNieXRlcy5jb20iLCJ1dGkiOiJydkJJSjhBNWZFT3VGUTJHR0xyZkFBIiwidmVyIjoiMS4wIiwid2lkcyI6WyIxOTRhZTRjYi1iMTI2LTQwYjItYmQ1Yi02MDkxYjM4MDk3N2QiLCJlODYxMWFiOC1jMTg5LTQ2ZTgtOTRlMS02MDIxM2FiMWY4MTQiLCI2MmU5MDM5NC02OWY1LTQyMzctOTE5MC0wMTIxNzcxNDVlMTAiLCJiNzlmYmY0ZC0zZWY5LTQ2ODktODE0My03NmIxOTRlODU1MDkiXSwieG1zX3N0Ijp7InN1YiI6Imdhb3RXUi1Oc0I3Nmktek90N3JPeFBxVkZPMkhiN0VJaWY2Z0hvSmtoaG8ifSwieG1zX3RjZHQiOjE1NTg4Mjc3OTF9.W5Wbl_00H1gLPcR0leUnQrfiPuycfRuvFDqTojr7bv4Q9mlQ0RYDwxyVJGQHUzolNxj0-6De_RzB91QrHqlufAwZ2GlOKV-OPmYXqkuRZkhX9wI6Ty22mNGEsXeWAp0k-shdxR1F2g2onN48fn8gZ62R-Ff9c1yKPIjWSBBg9Rj0dGSHsA5Y14DSDSoCwuydCS7LPdQttRof-dq0knV36K47WgS7jr0kM2FG7PakPPXT8HW4aFqznFOuB-4g59TZ-CNvayF-AfS_-E_VhbFbApCRR2wLv_hHe9cmc-zyN_QTwWi6c1fiSeerEKWStKEY8px4b6ykceR2e1tg8gka5w";
+        }
+
+        static GraphServiceClient Client(string accessToken)
+        {
+            return new GraphServiceClient(new DelegateAuthenticationProvider(async request => {
+                request.Headers.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
+            }));
+        }
+
         static void GetUnFilteredMessages(int qty = 5)
         {
-            var messages = _client.Me.Messages.Request().Top(qty).GetAsync().Result;
+            var client = Client(GetToken());
+
+            var messages = client.Me.Messages.Request().Top(qty).GetAsync().Result;
 
             foreach (var message in messages)
             {
@@ -32,7 +41,9 @@ namespace ExploringGraphAPI
 
         static void GetFilteredMessages(int qty = 3)
         {
-            var messages = _client.Me.Messages.Request()
+            var client = Client(GetToken());
+            
+            var messages = client.Me.Messages.Request()
                 .Select(m => new { m.Subject, m.SentDateTime })
                 .Filter("hasAttachments eq true")
                 .Expand(m => m.Attachments)
