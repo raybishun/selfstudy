5fc6c5ebeb7c0c96d27d74fa9b5016fe5d971cd0
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-11-29 18:57:05 -0500

    Modified GetAlertsTestMethod() to get JWT then MDE alerts

--- dev/cloud/azure/security/ADAL/MdeAdalMSTest/MdeAdalMSTest/UnitTest1.cs
+++ dev/cloud/azure/security/ADAL/MdeAdalMSTest/MdeAdalMSTest/UnitTest1.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 
 namespace MdeAdalMSTest
 {
@@ -9,15 +12,15 @@ namespace MdeAdalMSTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task GetAlertsTestMethod()
         {
             #region Notes
             // Token valid for 1 hour
-            // Decode token at: https://jwt.ms/
+            // Decode the JSON Web Token at: https://jwt.ms/
             #endregion
 
+            // Manage User-Secrets
             string path = @"C:\Users\raybi\AppData\Roaming\Microsoft\UserSecrets\c90d23dd-26f6-41e0-a8e4-d5e09e2f6850\secrets.json";
-
             IConfiguration config = new ConfigurationBuilder()
                 .AddJsonFile(path, true, true)
                 .Build();
@@ -29,12 +32,21 @@ namespace MdeAdalMSTest
             string authority = "https://login.windows.net";
             string wdatpResourceId = "https://api.securitycenter.windows.com";
 
+            // Get Token
             AuthenticationContext authContext = new AuthenticationContext($"{authority}/{tenantId}");
             ClientCredential clientCredential = new ClientCredential(appId, appSecret);
             AuthenticationResult authResult = authContext.AcquireTokenAsync(wdatpResourceId, clientCredential).GetAwaiter().GetResult();
-
             string token = authResult.AccessToken; 
-            Console.WriteLine(token);
+            Console.WriteLine($"{token}\n");
+
+            // Get MDE Alerts
+            string alertsRequestUri = "https://api.securitycenter.windows.com/api/alerts";
+            HttpClient httpClient = new HttpClient();
+            HttpRequestMessage httpRequest = new HttpRequestMessage(HttpMethod.Get, alertsRequestUri);
+            httpRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            HttpResponseMessage response = httpClient.SendAsync(httpRequest).GetAwaiter().GetResult();
+            string payload = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(payload);
         }
     }
 }
