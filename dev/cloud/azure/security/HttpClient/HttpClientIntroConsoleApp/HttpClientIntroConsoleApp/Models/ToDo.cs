10683364ecb128f6cfab92eb22d49182361285c8
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-11-30 20:07:07 -0500

    Added method to deserialize JSON returned from HttpClient

---
+++ dev/cloud/azure/security/HttpClient/HttpClientIntroConsoleApp/HttpClientIntroConsoleApp/Models/ToDo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpClientIntroConsoleApp.Models
+{
+    class ToDo
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+
+        public override string ToString()
+        {
+            return $"{UserId}, {Id}, {Title}, {Completed}";
+        }
+    }
+}
--- dev/cloud/azure/security/HttpClient/HttpClientIntroConsoleApp/HttpClientIntroConsoleApp/Program.cs
+++ dev/cloud/azure/security/HttpClient/HttpClientIntroConsoleApp/HttpClientIntroConsoleApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HttpClientIntroConsoleApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -6,28 +9,44 @@ namespace HttpClientIntroConsoleApp
 {
     class Program
     {
+        static readonly string _requestUri = "https://jsonplaceholder.typicode.com/todos";
+        static readonly HttpClient _client = new HttpClient();
+
         static async Task Main(string[] args)
         {
-            var results = await GetToDoItems();
-            Console.WriteLine(results);
+            await ShowToDoListAsync();
+            await ShowToDoListDeserializedAsync();
         }
 
-        static async Task<string> GetToDoItems()
+        static async Task ShowToDoListAsync()
         {
-            string uri = "https://jsonplaceholder.typicode.com/todos";
-            string response;
+            Console.WriteLine(await GetToDoListAsync());
+        }
 
-            try
-            {
-                HttpClient client = new HttpClient();
-                response = await client.GetStringAsync(uri);
-            }
-            catch (Exception ex)
-            {
-                response = ex.Message;
-            }
+        static async Task<string> GetToDoListAsync()
+        {
+            return await _client.GetStringAsync(_requestUri);
+        }
 
-            return response;
+
+        // ====================================================================
+        // Deserialized JSON
+        // ====================================================================
+        static async Task ShowToDoListDeserializedAsync()
+        {
+            List<ToDo> todoList = await GetToDoListDeserializedAsync();
+
+            foreach (var todo in todoList)
+            {
+                Console.WriteLine(todo.ToString());
+            }
+        }
+
+        static async Task<List<ToDo>> GetToDoListDeserializedAsync()
+        {
+            string results = await _client.GetStringAsync(_requestUri);
+
+            return JsonConvert.DeserializeObject<List<ToDo>>(results);
         }
     }
 }
