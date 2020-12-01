cb9be9bb9909913929e555d2fc92f79562eb9c58
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-11-30 19:01:09 -0500

    Added HttpClientIntroConsoleApp

---
+++ dev/cloud/azure/security/ADAL/AzureADAdalMSTest/AzureADAdalMSTest/UnitTest1.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace AzureADAdalMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task GetAlertsTestMethod()
+        {
+            #region Notes
+            // Token valid for 1 hour
+            // Decode the JSON Web Token at: https://jwt.ms/
+            #endregion
+
+            // Manage User-Secrets
+            string path = @"C:\Users\raybi\AppData\Roaming\Microsoft\UserSecrets\25abbabd-ee28-4702-831f-b1e783a6a2f1\secrets.json";
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile(path, true, true)
+                .Build();
+
+            string tenantId = config["TenantId"];
+            string appId = config["AppId"];
+            string appSecret = config["AppSecret"];
+
+            // string authority = $"https://login.windows.net/{tenantId}";
+            string authority = $"https://login.microsoftonline.com/{tenantId}";
+            
+            string resourceId = "https://graph.microsoft.com/";
+
+            // Get Token
+            AuthenticationContext authContext = new AuthenticationContext(authority);
+            ClientCredential clientCredential = new ClientCredential(appId, appSecret);
+            // AuthenticationResult authResult = authContext.AcquireTokenAsync(resourceId, clientCredential).GetAwaiter().GetResult();
+            AuthenticationResult authResult = await authContext.AcquireTokenAsync(resourceId, clientCredential);
+            string token = authResult.AccessToken;
+            Console.WriteLine($"{token}\n");
+
+            // Get AzureAD User Info
+            string requestUri = $"https://graph.microsoft.com/v1.0/users/ray.bishun@bitsbytes.com";
+            var client = new HttpClient();
+
+            //var httpRequest = new HttpRequestMessage(HttpMethod.Get, requestUri);
+            //httpRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            //var result = await client.SendAsync(httpRequest);
+            //Console.WriteLine(result);
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var result = await client.GetStringAsync(requestUri);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ dev/cloud/azure/security/HttpClient/HttpClientIntroConsoleApp/HttpClientIntroConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClientIntroConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var results = await GetToDoItems();
+            Console.WriteLine(results);
+        }
+
+        static async Task<string> GetToDoItems()
+        {
+            string uri = "https://jsonplaceholder.typicode.com/todos";
+            string response;
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                response = await client.GetStringAsync(uri);
+            }
+            catch (Exception ex)
+            {
+                response = ex.Message;
+            }
+
+            return response;
+        }
+    }
+}
