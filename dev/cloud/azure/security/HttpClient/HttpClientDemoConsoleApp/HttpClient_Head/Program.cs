cc852b34c8900882d0bb507e6c3eadaea6afcec4
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-12-04 22:28:51 -0500

    Added HttpClient_Header project

---
+++ dev/cloud/azure/security/HttpClient/HttpClientDemoConsoleApp/HttpClient_Head/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClient_Header
+{
+    class Program
+    {
+        static readonly string requestUri = "https://jsonplaceholder.typicode.com/todos";
+
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            HttpResponseMessage httpHeader = await GetResponseMessageAsync();
+            Console.WriteLine(httpHeader);
+        }
+
+        static async Task<HttpResponseMessage> GetResponseMessageAsync()
+        {
+            // Send an HTTP request
+            return await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, requestUri));
+        }
+    }
+}
