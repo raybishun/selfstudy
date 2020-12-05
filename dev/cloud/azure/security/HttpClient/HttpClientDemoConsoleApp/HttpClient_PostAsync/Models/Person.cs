a4449043809bbc5df4997faf6fe87fbe8f53c8a1
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-12-04 23:01:53 -0500

    Added HttpClient_PostAsync project

---
+++ dev/cloud/azure/security/HttpClient/HttpClientDemoConsoleApp/HttpClient_PostAsync/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace HttpClient_PostAsync.Models
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Occupation { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Occupation}";
+        }
+    }
+}
---
+++ dev/cloud/azure/security/HttpClient/HttpClientDemoConsoleApp/HttpClient_PostAsync/Program.cs
@@ -0,0 +1,38 @@
+﻿using HttpClient_PostAsync.Models;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClient_PostAsync
+{
+    class Program
+    {
+        static readonly string requestUri = "https://httpbin.org/post";
+
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            Person person = new Person
+            {
+                Name = "Peter Parker",
+                Occupation = "Superhero"
+            };
+
+            Console.WriteLine(await PostPersonAsync(person));
+        }
+
+        static async Task<string> PostPersonAsync(Person person)
+        {
+            string jsonResult = JsonConvert.SerializeObject(person);
+
+            StringContent data = new StringContent(jsonResult, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync(requestUri, data);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
