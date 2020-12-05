18a5e420a4f9d94179c67dfce0583f98a2c99c12
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-12-04 23:58:46 -0500

    Added HttpClient_DeserializeObject2 project

---
+++ dev/cloud/azure/security/HttpClient/HttpClientDemoConsoleApp/HttpClient_DeserializeObject2/Models/Contributor.cs
@@ -0,0 +1,13 @@
+﻿namespace HttpClient_DeserializeObject2.Models
+{
+    class Contributor
+    {
+        public string Login { get; set; }
+        public short Contributions { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Login,20}: {Contributions} contributions.";
+        }
+    }
+}
---
+++ dev/cloud/azure/security/HttpClient/HttpClientDemoConsoleApp/HttpClient_DeserializeObject2/Program.cs
@@ -0,0 +1,35 @@
+﻿using HttpClient_DeserializeObject2.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace HttpClient_DeserializeObject2
+{
+    class Program
+    {
+        static readonly string uriString = "https://api.github.com";
+
+        static async Task Main(string[] args)
+        {
+            using HttpClient client = new HttpClient
+            {
+                BaseAddress = new Uri(uriString),
+            };
+
+            client.DefaultRequestHeaders.Add("User-Agent", "My .NET Core Console App");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            string requestUri = "repos/symfony/symfony/contributors";
+            
+            HttpResponseMessage response = await client.GetAsync(requestUri);
+            response.EnsureSuccessStatusCode();
+            var strResult = await response.Content.ReadAsStringAsync();
+
+            List<Contributor> contributors = JsonConvert.DeserializeObject<List<Contributor>>(strResult);
+            contributors.ForEach(Console.WriteLine);
+        }
+    }
+}
