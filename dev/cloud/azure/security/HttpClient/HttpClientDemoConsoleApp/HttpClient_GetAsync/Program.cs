96b43b61ada8275c5f19c7cee87c64b355211de4
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-12-04 20:53:13 -0500

    Added HttpClient_GetAsync project

---
+++ dev/cloud/azure/security/HttpClient/HttpClientDemoConsoleApp/HttpClient_GetAsync/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClient_GetAsync
+{
+    class Program
+    {
+        static readonly string requestUri = "https://jsonplaceholder.typicode.com/todos";
+
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            HttpResponseMessage data = await GetResponseMessageAsync();
+
+            // Get status code
+            Console.WriteLine(data.StatusCode);
+            
+            // Get data
+            Console.WriteLine(await data.Content.ReadAsStringAsync());
+        }
+
+        static async Task<HttpResponseMessage> GetResponseMessageAsync()
+        {
+            // Returns the actual data along with status codes
+            return await client.GetAsync(requestUri);
+        }
+    }
+}
