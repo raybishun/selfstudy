cc67cad94f97ed50be23b623c779d48fc72450b6
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-12-05 20:37:11 -0500

    Added HttpClient_BasicAuth project

--- dev/cloud/azure/security/HttpClient/HttpClientDemoConsoleApp/HttpClient_BasicAuth/Program.cs
+++ dev/cloud/azure/security/HttpClient/HttpClientDemoConsoleApp/HttpClient_BasicAuth/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace HttpClient_BasicAuth
 {
@@ -13,17 +17,28 @@ namespace HttpClient_BasicAuth
         // Uses standard fields in the HTTP header
         #endregion
 
-        static readonly string requestUri = "https://httpbin.org/basic-auth/user7/passwd";
-        static readonly HttpClient client = new HttpClient();
-
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            
+            string path = @"C:\Users\raybi\AppData\Roaming\Microsoft\UserSecrets\53ff3096-ab13-4b9f-9c5c-c2e5583befa6\secrets.json";
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile(path, true, true)
+                .Build();
+
+            string user = config["UserName"];
+            string passwd = config["Passwd"];
+
+            HttpClient client = new HttpClient();
+            string requestUri = "https://httpbin.org/basic-auth/user7/passwd";
+
+            Console.WriteLine(await BasicAuthAsync(client, requestUri, user, passwd));
         }
 
-        static void DoWork()
-        { 
-        
+        static async Task<string> BasicAuthAsync(HttpClient client, string requestUri, string user, string passwd)
+        {
+            byte[] authToken = Encoding.ASCII.GetBytes($"{user}:{passwd}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
+            HttpResponseMessage result = await client.GetAsync(requestUri);
+            return await result.Content.ReadAsStringAsync();
         }
     }
 }
