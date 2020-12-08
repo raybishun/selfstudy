b3cc93ca84a285284a48cf7a15548fdbc39a1b0b
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-12-07 20:02:51 -0500

    Init Commit for TopsyTurvyCakes2

--- dev/languages/web_back-end/RazorPages/TopsyTurvyCakes2/TopsyTurvyCakes2/Startup.cs
+++ dev/languages/web_back-end/RazorPages/TopsyTurvyCakes2/TopsyTurvyCakes2/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,9 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-namespace TopsyTurvyCakes
+namespace TopsyTurvyCakes2
 {
     public class Startup
     {
@@ -16,25 +15,23 @@ namespace TopsyTurvyCakes
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
